b0c258fa23e3f26f1d8dad15cb1dffcd4b4263d7
Chen Wu <chen.wu@icrar.org>
2015-06-18 00:48:25 +0800

    use Luigi and NGAS to "realise" graph engine and data manager respectively. Currently mixing graph deployment and execution, need to separate them!

---
+++ test/dfmgr/__init__.py
@@ -0,0 +1,7 @@
+"""
+The module that simulates LMC dataflow manager
+to deploy and schedule physical graph across multiple data managers / islands
+
+It is implemented as a combination of
+(1) a graph execution engine (Luigi) and data (object) manager (NGAS)
+"""
