d23704c4e7a6566da3c4bf9f842d0355738a8e20
Jason Wang <jason.wang@icrar.org>
2015-07-07 03:57:09 +0800

    replaced the thousands-of-lines C++ ms_converter code with a 20 lines python code using python-casacore

---
+++ test/integrate/msconverter/ms_converter.py
@@ -0,0 +1,19 @@
+import pyrap.tables as pt
+
+oldtable='/scratch/jason/chiles_original.ms'
+newtable='/scratch/jason/chiles_adios.ms'
+t = pt.table(oldtable)
+dmdef = t.getdminfo()
+
+print 'Original Table: **************************'
+for i in dmdef:
+	print i
+	print dmdef[i]
+
+dmdef['*17']['TYPE'] = 'AdiosStMan'
+print 'New Table: ***********************'
+for i in dmdef:
+	print i
+	print dmdef[i]
+
+t.copy (newtable, True, True, dminfo=dmdef)
