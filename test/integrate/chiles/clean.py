a4225ac66fae110dc8ce3230de2f7320500a928e
Dave P <username@users.noreply.github.com>
2015-08-19 12:18:43 +0800

    Added chiles imaging pipeline files

---
+++ test/integrate/chiles/clean.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+
+def do_clean(infiles, outfile):
+
+    if not infiles:
+        raise Exception('infiles is empty')
+
+    if not outfile:
+        raise Exception('outfile is empty')
+
+    clean(vis=infiles,
+        imagename=outfile,
+        field='deepfield',
+        spw='',
+        mode='frequency',
+        restfreq='1420.405752MHz',
+        nchan=-1,
+        start='',
+        width='',
+        interpolation='nearest',
+        niter=0,
+        gain=0.1,
+        threshold='0.0mJy',
+        imsize=[256],
+        cell=['1.0arcsec'],
+        phasecenter='10h01m53.9,+02d24m52s',
+        weighting='natural',
+        usescratch=False)
+
+
+if __name__ == '__main__':
+
+    try:
+        print indir
+        inputs = []
+        path, files, _ = next(os.walk(indir))
+        for f in files:
+            inputs.append(path + '/' + f)
+
+        print inputs
+        print outcube
+        do_clean(inputs, outcube)
+
+    except Exception as ex:
+        print >> sys.stderr, 'SEVERE ' + str(ex)
+
