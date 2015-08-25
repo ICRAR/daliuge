7c2adde35e83d8d29d340cfea75019daa2d75d7c
Chen Wu <chen.wu@icrar.org>
2015-08-25 15:42:59 +0800

    change trigger text to "Ingest"

--- test/dfmgr/pg_web.py
+++ test/dfmgr/pg_web.py
@@ -19,7 +19,7 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #    MA 02111-1307  USA
 #
-import json, decimal
+import json, decimal, urllib2
 import subprocess, commands
 from test import graphsRepository
 import threading
