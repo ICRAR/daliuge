3dc7031c03c0963152e3540a94f35851a73b38fa
Rodrigo Tobar <rtobar@icrar.org>
2015-10-09 12:36:34 +0800

    New graph_loader.removeUnmetRelationships method
    This is necessary for the implementation of the Data Island Manager, which needs to break down a full graph into individual, self-contained per-DOM graphs.

--- test/test_graph_loader.py
+++ test/test_graph_loader.py
@@ -25,6 +25,7 @@ import unittest
 from dfms import graph_loader
 from dfms.data_object import InMemoryDataObject, ContainerDataObject,\
     AppDataObject
+from dfms.ddap_protocol import DOLinkType, DORel
 
 # Used in the textual representation of the graphs in these tests
 class DummyApp(AppDataObject): pass
@@ -64,3 +65,30 @@ class TestGraphLoader(unittest.TestCase):
         self.assertEquals("B", b.oid)
         self.assertEquals("B", b.uid)
         self.assertEquals(a, b.inputs[0])
+
+    def test_removeUnmetRelationships(self):
+
+        # Unmet relationsips are
+        # DORel(D, CONSUMER, A)
+        # DORel(D, STREAMING_CONSUMER, C)
+        # DORel(Z, PRODUCER, A)
+        # DORel(X, PRODUCER, A)
+        graphDesc = [{'oid':'A', 'consumers':['B', 'D'], 'producers':['Z','X']},
+                     {'oid':'B', 'outputs':['C']},
+                     {'oid':'C', 'streamingConsumers':['D']}]
+
+        unmetRelationships = graph_loader.removeUnmetRelationships(graphDesc)
+        self.assertEquals(4, len(unmetRelationships))
+        self.assertIn(DORel('D', DOLinkType.CONSUMER, 'A'), unmetRelationships)
+        self.assertIn(DORel('D', DOLinkType.STREAMING_CONSUMER, 'C'), unmetRelationships)
+        self.assertIn(DORel('Z', DOLinkType.PRODUCER, 'A'), unmetRelationships)
+        self.assertIn(DORel('X', DOLinkType.PRODUCER, 'A'), unmetRelationships)
+
+        # The original doSpecs have changed as well
+        a = graphDesc[0]
+        c = graphDesc[2]
+
+        self.assertEquals(1, len(a['consumers']))
+        self.assertEquals('B', a['consumers'][0])
+        self.assertFalse('producers' in a)
+        self.assertFalse('streamingConsumers' in c)
