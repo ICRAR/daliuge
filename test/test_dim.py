756d23111f27cf55f4e61a8c6ac8c1c7dcc0c996
Rodrigo Tobar <rtobar@icrar.org>
2015-10-12 14:26:56 +0800

    More methods and tests for the DIM
    getGraphStatus, getGraph and getSessionStatus have been added to the DIM, which in turn gathers the results of the individual DOMs. The gathering happens in separate threads. Unit tests have been added for the new methods.
    
    Additionally, proper error propagation has been added at the DIM level when parallel executions take place to invoke DOM methods. Exceptions are collected per-DOM, and if any is found after all threads finish a new exception is raised in the DIM method caller thread to show that there was an error in some of the per-DOM threads.

--- test/test_dim.py
+++ test/test_dim.py
@@ -33,6 +33,8 @@ from dfms import doutils
 from dfms.dim.data_island_manager import DataIslandManager
 from dfms.dom.data_object_mgr import DataObjectMgr
 from dfms.utils import portIsOpen
+from dfms.dom.session import SessionStates
+from dfms.ddap_protocol import DOStates
 
 
 dimId = 'lala'
@@ -56,13 +58,13 @@ class TestDIM(unittest.TestCase):
         # Anyway, this is also useful because we can check that things have
         # occurred at the DOM level in the test cases
         domId = 'dom_' + hostname
-        self._dom = DataObjectMgr(domId, False)
+        self.dom = DataObjectMgr(domId, False)
         self._domDaemon = Pyro4.Daemon(host='0.0.0.0', port=4000)
-        self._nsDaemon.nameserver.register(domId, self._domDaemon.register(self._dom))
+        self._nsDaemon.nameserver.register(domId, self._domDaemon.register(self.dom, objectId='DOM_for_test'))
         threading.Thread(target=lambda: self._domDaemon.requestLoop()).start()
 
         # The DIM we're testing
-        self._dim = DataIslandManager(dimId, [hostname])
+        self.dim = DataIslandManager(dimId, [hostname])
 
     def tearDown(self):
         self._domDaemon.shutdown()
@@ -70,33 +72,36 @@ class TestDIM(unittest.TestCase):
         while portIsOpen('localhost', Pyro4.config.NS_PORT):
             time.sleep(0.01)
 
+    def createSessionAndAddTypicalGraph(self, sessionId):
+        graphSpec = [{'oid':'A', 'type':'plain', 'storage':'memory', 'location':hostname, 'consumers':['B']},
+                     {'oid':'B', 'type':'app', 'app':'test.graphsRepository.SleepAndCopyApp', 'sleepTime':0, 'outputs':['C'], 'location':hostname},
+                     {'oid':'C', 'type':'plain', 'storage':'memory', 'location':hostname}]
+        self.dim.createSession(sessionId)
+        self.dim.addGraphSpec(sessionId, graphSpec)
+
     def test_createSession(self):
-
         sessionId = 'lalo'
-        dim = self._dim
-
-        dim.createSession(sessionId)
-        self.assertEquals(1, len(self._dom.getSessionIds()))
-        self.assertEquals(sessionId, self._dom.getSessionIds()[0])
+        self.dim.createSession(sessionId)
+        self.assertEquals(1, len(self.dom.getSessionIds()))
+        self.assertEquals(sessionId, self.dom.getSessionIds()[0])
 
     def test_addGraphSpec(self):
 
         sessionId = 'lalo'
-        dim = self._dim
 
         # No location specified
         graphSpec = [{'oid':'A', 'type':'plain', 'storage':'memory'}]
-        self.assertRaises(Exception, dim.addGraphSpec, sessionId, graphSpec)
+        self.assertRaises(Exception, self.dim.addGraphSpec, sessionId, graphSpec)
 
         # Wrong location specified
         graphSpec = [{'oid':'A', 'type':'plain', 'storage':'memory', 'location':'unknown_host'}]
-        self.assertRaises(Exception, dim.addGraphSpec, sessionId, graphSpec)
+        self.assertRaises(Exception, self.dim.addGraphSpec, sessionId, graphSpec)
 
         # OK
         graphSpec = [{'oid':'A', 'type':'plain', 'storage':'memory', 'location':hostname}]
-        dim.createSession(sessionId)
-        dim.addGraphSpec(sessionId, graphSpec)
-        graphFromDOM = self._dom.getGraph(sessionId)
+        self.dim.createSession(sessionId)
+        self.dim.addGraphSpec(sessionId, graphSpec)
+        graphFromDOM = self.dom.getGraph(sessionId)
         self.assertEquals(1, len(graphFromDOM))
         doSpec = graphFromDOM.values()[0]
         self.assertEquals('A', doSpec['oid'])
@@ -106,23 +111,79 @@ class TestDIM(unittest.TestCase):
     def test_deployGraph(self):
 
         sessionId = 'lalo'
-        graphSpec = [{'oid':'A', 'type':'plain', 'storage':'memory', 'location':hostname, 'consumers':['B']},
-                     {'oid':'B', 'type':'app', 'app':'test.graphsRepository.SleepAndCopyApp', 'sleepTime':0, 'outputs':['C'], 'location':hostname},
-                     {'oid':'C', 'type':'plain', 'storage':'memory', 'location':hostname}]
-        dim = self._dim
-
-        dim.createSession(sessionId)
-        dim.addGraphSpec(sessionId, graphSpec)
+        self.createSessionAndAddTypicalGraph(sessionId)
 
         # Deploy now and get the uris. With that we get then A's and C's proxies
-        uris = dim.deploySession(sessionId)
+        uris = self.dim.deploySession(sessionId)
         a = Pyro4.Proxy(uris['A'])
         c = Pyro4.Proxy(uris['C'])
 
         data = ''.join([random.choice(string.ascii_letters + string.digits) for _ in xrange(10)])
-
         with doutils.EvtConsumerProxyCtx(self, c, 3):
             a.write(data)
             a.setCompleted()
 
         self.assertEquals(data, doutils.allDataObjectContents(c))
+
+    def test_sessionStatus(self):
+
+        def assertSessionStatus(sessionId, status):
+            sessionStatus = self.dim.getSessionStatus(sessionId)
+            self.assertEquals(1, len(sessionStatus))
+            self.assertIn(hostname, sessionStatus)
+            self.assertEquals(status, sessionStatus[hostname])
+            self.assertEquals(status, self.dom.getSessionStatus(sessionId))
+
+        sessionId = 'lala'
+        self.dim.createSession(sessionId)
+        assertSessionStatus(sessionId, SessionStates.PRISTINE)
+
+        sessionId = 'lalo'
+        self.createSessionAndAddTypicalGraph(sessionId)
+        assertSessionStatus(sessionId, SessionStates.BUILDING)
+
+        uris = self.dim.deploySession(sessionId)
+        assertSessionStatus(sessionId, SessionStates.RUNNING)
+
+        a = Pyro4.Proxy(uris['A'])
+        c = Pyro4.Proxy(uris['C'])
+        data = ''.join([random.choice(string.ascii_letters + string.digits) for _ in xrange(10)])
+        with doutils.EvtConsumerProxyCtx(self, c, 3):
+            a.write(data)
+            a.setCompleted()
+
+        assertSessionStatus(sessionId, SessionStates.FINISHED)
+
+    def test_getGraph(self):
+
+        sessionId = 'lalo'
+        self.createSessionAndAddTypicalGraph(sessionId)
+
+        graphSpecFromDim = self.dim.getGraph(sessionId)
+        self.assertEquals(3, len(graphSpecFromDim))
+        for oid in ('A','B','C'):
+            self.assertIn(oid, graphSpecFromDim)
+        graphSepcFromDom = self.dom.getGraph(sessionId)
+        self.assertDictEqual(graphSepcFromDom, graphSpecFromDim)
+
+    def test_getGraphStatus(self):
+
+        def assertGraphStatus(sessionId, expectedStatus):
+            graphStatusByDim = self.dim.getGraphStatus(sessionId)
+            graphStatusByDom = self.dom.getGraphStatus(sessionId)
+            self.assertDictEqual(graphStatusByDim, graphStatusByDom)
+            for doStatus in graphStatusByDim.viewvalues():
+                self.assertEquals(expectedStatus, doStatus)
+
+        sessionId = 'lala'
+        self.createSessionAndAddTypicalGraph(sessionId)
+        uris = self.dim.deploySession(sessionId)
+        assertGraphStatus(sessionId, DOStates.INITIALIZED)
+
+        a = Pyro4.Proxy(uris['A'])
+        c = Pyro4.Proxy(uris['C'])
+        data = ''.join([random.choice(string.ascii_letters + string.digits) for _ in xrange(10)])
+        with doutils.EvtConsumerProxyCtx(self, c, 3):
+            a.write(data)
+            a.setCompleted()
+        assertGraphStatus(sessionId, DOStates.COMPLETED)
