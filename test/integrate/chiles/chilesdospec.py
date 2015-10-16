40fcdb05d6f5f9dc0f9087511876090cc44475d3
Rodrigo Tobar <rtobar@icrar.org>
2015-10-16 13:35:49 +0800

    Simplified chilesdospec, generalized it a bit

--- test/integrate/chiles/chilesdospec.py
+++ test/integrate/chiles/chilesdospec.py
@@ -19,13 +19,13 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #    MA 02111-1307  USA
 #
+import json
 import os
 import sys
 import uuid
-import threading
-import json
-from chilesdo import Split, Clean, SourceFlux
-from dfms.data_object import DirectoryContainer, BarrierAppDataObject, InMemoryDataObject, dodict
+
+from dfms.data_object import dodict
+
 
 LOCAL_FILES = os.path.dirname(os.path.realpath(__file__))
 CASAPY = '/home/ec2-user/casa-release-4.4.0-el6/'
@@ -35,12 +35,17 @@ CUBE_OUT = '/home/ec2-user/data/output/'
 CUBE_NAME = 'cube1408~1412'
 KEY = '/home/ec2-user/.ssh/aws-sdp-sydney.pem'
 
-# 172.31.2.74 172.31.5.15 172.31.5.14 172.31.5.16 172.31.5.13
+# Internal AWS IP addresses. ch05 does the Clean-ing
+ch01 = '172.31.4.12'
+ch02 = '172.31.9.163'
+ch03 = '172.31.11.184'
+ch04 = '172.31.11.87'
+ch05 = '172.31.0.36'
 VIS = [
-        (VIS_ROOT + '20131025_951_4_FINAL_PRODUCTS/20131025_951_4_calibrated_deepfield.ms', VIS_OUT + '20131025_951_4/', '172.31.5.15', 'ec2-user@172.31.2.74:' + VIS_ROOT),
-        (VIS_ROOT + '20131031_951_4_FINAL_PRODUCTS/20131031_951_4_calibrated_deepfield.ms', VIS_OUT + '20131031_951_4/', '172.31.5.14', 'ec2-user@172.31.2.74:' + VIS_ROOT),
-        (VIS_ROOT + '20131121_946_6_FINAL_PRODUCTS/20131121_946_6_calibrated_deepfield.ms', VIS_OUT + '20131121_946_6/', '172.31.5.16', 'ec2-user@172.31.2.74:' + VIS_ROOT),
-        (VIS_ROOT + '20140105_946_6_FINAL_PRODUCTS/20140105_946_6_calibrated_deepfield.ms', VIS_OUT + '20140105_946_6/', '172.31.5.13', 'ec2-user@172.31.2.74:' + VIS_ROOT)
+        (VIS_ROOT + '20131025_951_4_FINAL_PRODUCTS/20131025_951_4_calibrated_deepfield.ms', VIS_OUT + '20131025_951_4/', ch01, 'ec2-user@' + ch05 + ':' + VIS_ROOT),
+        (VIS_ROOT + '20131031_951_4_FINAL_PRODUCTS/20131031_951_4_calibrated_deepfield.ms', VIS_OUT + '20131031_951_4/', ch03, 'ec2-user@' + ch05 + ':' + VIS_ROOT),
+        (VIS_ROOT + '20131121_946_6_FINAL_PRODUCTS/20131121_946_6_calibrated_deepfield.ms', VIS_OUT + '20131121_946_6/', ch04, 'ec2-user@' + ch05 + ':' + VIS_ROOT),
+        (VIS_ROOT + '20140105_946_6_FINAL_PRODUCTS/20140105_946_6_calibrated_deepfield.ms', VIS_OUT + '20140105_946_6/', ch02, 'ec2-user@' + ch05 + ':' + VIS_ROOT)
         ]
 
 
@@ -79,11 +84,9 @@ if __name__ == '__main__':
 
         dolist = []
 
-        vis_in_a = []
-
-        flux_out = memorySpec(uuid.uuid1(), location = '172.31.2.74')
+        flux_out = memorySpec(uuid.uuid1(), location = ch05)
         dolist.append(flux_out)
-        flux = fluxSpec(uuid.uuid1(), casapy_path = CASAPY, location = '172.31.2.74')
+        flux = fluxSpec(uuid.uuid1(), casapy_path = CASAPY, location = ch05)
         dolist.append(flux)
 
         cl = cleanSpec(uuid.uuid1(),
@@ -100,11 +103,11 @@ if __name__ == '__main__':
                         phasecenter = '10h01m53.9,+02d24m52s',
                         weighting = 'natural',
                         casapy_path = CASAPY,
-                        location = '172.31.2.74')
+                        location = ch05)
 
         dolist.append(cl)
 
-        image_out = directorySpec(uuid.uuid1(), dirname = CUBE_OUT + CUBE_NAME, exists = False, location = '172.31.2.74')
+        image_out = directorySpec(uuid.uuid1(), dirname = CUBE_OUT + CUBE_NAME, exists = False, location = ch05)
         dolist.append(image_out)
         cl.addOutput(image_out)
         flux.addInput(image_out)
@@ -115,8 +118,6 @@ if __name__ == '__main__':
             dolist.append(vis_in)
             split_out = directorySpec(uuid.uuid1(), dirname = v[1], exists = False, location = v[2])
             dolist.append(split_out)
-
-            vis_in_a.append(vis_in)
 
             sp = splitSpec(uuid.uuid1(), 
                         regridms = True, 
@@ -139,9 +140,7 @@ if __name__ == '__main__':
             sp.addOutput(split_out)
             cl.addInput(split_out)
 
-
         print json.dumps(dolist)
-
 
     except Exception as e:
         import traceback
