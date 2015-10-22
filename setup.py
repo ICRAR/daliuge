25e61039a5fd60be2c76f1c27774fecf9c85bb00
Rodrigo Tobar <rtobar@icrar.org>
2015-10-22 10:33:04 +0800

    Adding spead2 dependency, still experimental

--- setup.py
+++ setup.py
@@ -3,6 +3,32 @@
 from setuptools import setup
 from setuptools import find_packages
 
+# HACK - HACK - HACK - HACK
+#
+# We externally make sure that numpy is installed because spead2 needs it there
+# at compile time (and therefore at runtime too).o
+
+# An initial solution for this problem was to add numpy to the setup_requires
+# argument of spead2's setup invocation. This solves the problem of compiling
+# numpy, but requires some extra code to make numpy's include directory
+# (which isn't installed in a standard location, and therefore must be queried
+# to numpy itself via numpy.include_dir()) available to spead2's setup in order
+# to build its C extensions correctly. The main drawback from this solution is
+# that numpy's egg location remains inside the spead2's source code tree (in the
+# root of the tree when using setuptools < 7, inside the .eggs/ directory when
+# using setuptools>=7). This was reported in setuptool issues #209 and #391, but
+# still remains an issue. Although one could live with such an installation, it
+# doesn't sound ideal at all since the software is not installed when one would
+# expect it to be; also permissions-based problems could arise.
+#
+# For the time being I'm choosing instead to simply install numpy via a pip
+# command-line invocation. It will avoid any numpy mingling by spead2, and will
+# return quickly if it has been already installed
+#
+# HACK - HACK - HACK - HACK
+import subprocess
+subprocess.check_call(['pip','install','numpy'])
+
 setup(
       name='dfms',
       version='0.1',
@@ -14,8 +40,9 @@ setup(
       package_data = {
         'dfms.manager' : ['web/*.html', 'web/static/css/*.css', 'web/static/fonts/*', 'web/static/js/*.js', 'web/static/js/d3/*']
       },
-      install_requires=["Pyro4", "luigi", "psutil", "paramiko", "bottle", "tornado", "drive-casa", "docker-py"],
-      dependency_links=["https://github.com/davepallot/drive-casa/archive/0.6.7.zip#egg=drive-casa-0.6.7"],
+      install_requires=["Pyro4", "luigi", "psutil", "paramiko", "bottle", "tornado", "drive-casa", "docker-py","spead2==0.4.0"],
+      dependency_links=["https://github.com/davepallot/drive-casa/tarball/0.6.7#egg=drive-casa-0.6.7",
+                        "https://github.com/ska-sa/spead2/tarball/v0.4.0#egg=spead2-0.4.0"],
       test_suite="test",
       entry_points= {
           'console_scripts':[
