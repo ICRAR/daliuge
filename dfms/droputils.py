93f9f0f0f90bde7003e7d04bf50dde613fd02c96
Rodrigo Tobar <rtobar@icrar.org>
2015-12-02 13:50:33 +0800

    Small TODO comment added to EvtConsumer()

--- dfms/droputils.py
+++ dfms/droputils.py
@@ -43,7 +43,21 @@ class EvtConsumer(object):
     '''
     Small utility class that sets the internal flag of the given threading.Event
     object when consuming a DROP. Used throughout the tests as a barrier to wait
-    until all DROPs of a given graph have executed
+    until all DROPs of a given graph have executed.
+
+    TODO: This class is currently connected to each DROP by calling
+          drop.addConsumer(evtConsumer). This goes against the idea of having
+          method calls only between DROPs, while external entities simply listen
+          to events. The bad news is that the default serializer class used by
+          Pyro >= 4.20 is the 'serpent' serializer, not the 'pickle' one. In the
+          'serpent' serializer "most custom classes aren't dealt with
+          automatically", as shown in the Pyro documentation (see link below).
+          Thus, in order to support passing events via Pyro we'd need to
+          manually configure all our Pyro systems (manually tested, it works)
+          with:
+
+           Pyro4.config.SERIALIZER = 'pickle'
+           Pyro4.config.SERIALIZERS_ACCEPTED = ['pickle']
     '''
     def __init__(self, evt):
         self._evt = evt
