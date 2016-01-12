b5f1dae906882b3944a69b6c990c38d9c16aa583
Chen Wu <chen.wu@icrar.org>
2016-01-12 18:07:05 +0800

    add icon resources

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
       packages=find_packages(),
       package_data = {
         'dfms.manager' : ['web/*.html', 'web/static/css/*.css', 'web/static/fonts/*', 'web/static/js/*.js', 'web/static/js/d3/*'],
-        'dfms.lg': ['web/lg_editor.html', 'web/*.css', 'web/*.js', 'web/*.json', 'web/*.map', 'web/pg_viewer.html']
+        'dfms.lg': ['web/lg_editor.html', 'web/*.css', 'web/*.js', 'web/*.json', 'web/*.map', 'web/img/jsoneditor-icons.png', 'web/pg_viewer.html']
       },
 
       # Keep alpha-sorted please
