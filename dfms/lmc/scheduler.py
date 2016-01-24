6565613ef84349c4a4c4039ce8c0108798b7d8df
Chen Wu <chen.wu@icrar.org>
2016-01-25 00:32:30 +0800

    scheduler based on Sarkar and DSC methods (first cut)

---
+++ dfms/lmc/scheduler.py
@@ -0,0 +1,166 @@
+
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia, 2015
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+
+import networkx as nx
+
+class Schedule(object):
+    """
+    The scheduling solution object, which contains:
+    which drop is executed at where [and when]
+    """
+    @property
+    def makespan(self):
+        pass
+
+    def to_gojs_json(self):
+        pass
+
+
+class Scheduler(object):
+    """
+    Static Scheduling consists of three steps:
+    1. partition the DAG into an optimal number (M) of clusters
+    2. merge clusters into a given number (N) of resource units (if M > N)
+    3. map each cluster to a resource unit
+    """
+
+    def __init__(self, drop_list):
+        """
+        turn drop_list into DAG, and check its validity
+        """
+        pass
+
+    def partition_dag(self):
+        pass
+
+    def merge_cluster(self, num_partitions):
+        """
+        Return a schedule
+        """
+        pass
+
+    def map_cluster(self):
+        pass
+
+class SarkarScheduler(Scheduler):
+    """
+    Based on
+    V. Sarkar, Partitioning and Scheduling Parallel Programs for Execution on
+    Multiprocessors. Cambridge, MA: MIT Press, 1989.
+    """
+    def __init__(self, drop_list):
+        super(SarkarScheduler, self).__init__(drop_list)
+
+    def partition_dag(self):
+        pass
+
+    def merge_cluster(self, num_partitions):
+        pass
+
+    def map_cluster(self):
+        pass
+
+class DSCScheduler(Schedule):
+    """
+    Based on
+    T. Yang and A. Gerasoulis, "DSC: scheduling parallel tasks on an
+    unbounded number of processors," in IEEE Transactions on
+    Parallel and Distributed Systems, vol.5, no.9, pp.951-967, Sep 1994
+    """
+    def __init__(self, drop_list):
+        super(DSCScheduler, self).__init__(drop_list)
+
+    def partition_dag(self):
+        pass
+
+    def merge_cluster(self, num_partitions):
+        pass
+
+    def map_cluster(self):
+        pass
+
+class DAGUtil(object):
+    """
+    Helper functions dealing with DAG
+    """
+    @staticmethod
+    def get_longest_path(G, weight='weight', default_weight=1, show_path=True):
+        """
+        Ported from:
+        https://github.com/networkx/networkx/blob/master/networkx/algorithms/dag.py
+        To add node weight
+
+        Returns the longest path in a DAG
+        If G has edges with 'weight' attribute the edge data are used as weight values.
+        Parameters
+        ----------
+        G : NetworkX DiGraph
+            Graph
+        weight : string (default 'weight')
+            Edge data key to use for weight
+        default_weight : integer (default 1)
+            The weight of edges that do not have a weight attribute
+        Returns a tuple with two elements
+        -------
+        path : list
+            Longest path
+        path_length : float
+            The length of the longest path
+
+        """
+        dist = {} # stores {v : (length, u)}
+        for v in nx.topological_sort(G):
+            us = [
+            (dist[u][0] + #accumulate
+            data.get(weight, default_weight) + #edge weight
+            G.node[u].get(weight, 0) + # u node weight
+            (G.node[v].get(weight, 0) if len(G.successors(v)) == 0 else 0), # v node weight if no successor
+            u)
+                for u, data in G.pred[v].items()]
+            # Use the best predecessor if there is one and its distance is non-negative, otherwise terminate.
+            maxu = max(us) if us else (0, v)
+            dist[v] = maxu if maxu[0] >= 0 else (0, v)
+        u = None
+        v = max(dist, key=dist.get)
+        lp = dist[v][0]
+        if (not show_path):
+            path = None
+        else:
+            path = []
+            while u != v:
+                path.append(v)
+                u = v
+                v = dist[v][1]
+            path.reverse()
+        return (path, lp)
+
+if __name__ == "__main__":
+    G = nx.DiGraph()
+    G.add_weighted_edges_from([(4,3,1.2), (3,2,4), (2,1,2)])
+    G.add_weighted_edges_from([(3,6,5.2), (6,7,2)])
+    G.node[7]['weight'] = 65
+    print G.nodes(data=True)
+    print G.edges(data=True)
+
+    lp = DAGUtil.get_longest_path(G)
+
+    print "The longest path is {0} with a length of {1}".format(lp[0], lp[1])
