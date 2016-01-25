22b3a8cea4c89dcee5166dbb0d79490944efae29
Rodrigo Tobar <rtobar@icrar.org>
2016-01-25 09:43:19 +0800

    Skip the S3 tests if user is not ready for them

--- test/test_S3Drop.py
+++ test/test_S3Drop.py
@@ -19,17 +19,29 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #    MA 02111-1307  USA
 #
+from unittest.case import skipIf
 """
 Test the S3 Drop
 """
+# If the profile is not present in the current user's account
+# we simply skip the test
+
 import unittest
 
+import boto3
 from dfms.s3_drop import S3DROP
 
-PROFILE = 'aws-chiles02'
 
+PROFILE = 'aws-chiles02'
+run_tests = True
+try:
+    boto3.session.Session(profile_name=PROFILE)
+except:
+    run_tests = False
 
 class TestS3Drop(unittest.TestCase):
+
+    @skipIf(not run_tests, "No profile found to run this test")
     def test_bucket_exists(self):
         drop = S3DROP('oid:A', 'uid:A', profile_name=PROFILE, bucket='DoesNotExist', key='Nonsense')
         self.assertEquals(drop.exists(), False)
@@ -40,13 +52,10 @@ class TestS3Drop(unittest.TestCase):
         drop = S3DROP('oid:A', 'uid:A', profile_name=PROFILE, bucket='13b-266', key='13B-266.sb25386827.eb28551343.56619.33367407408_calibrated_deepfield.ms.tar')
         self.assertEquals(drop.exists(), True)
 
+    @skipIf(not run_tests, "No profile found to run this test")
     def test_size(self):
         drop = S3DROP('oid:A', 'uid:A', profile_name=PROFILE, bucket='DoesNotExist', key='Nonsense')
         self.assertEquals(drop.size(), -1)
 
         drop = S3DROP('oid:A', 'uid:A', profile_name=PROFILE, bucket='13b-266', key='13B-266.sb25386827.eb28551343.56619.33367407408_calibrated_deepfield.ms.tar')
         self.assertEquals(drop.size(), 734067056640)
-
-
-if __name__ == '__main__':
-    unittest.main()
