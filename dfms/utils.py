050403cb6ea54bfb65cef93276cf2c27c6223ec9
Rodrigo Tobar <rtobar@icrar.org>
2016-03-21 11:52:00 +0800

    Logs go to configurable directory
    We also don't redirect the stdout/stderr of the Drop Managers anymore when
    starting them as daemons; otherwise that would clash with the logfile that
    we're now always creating using python's logging system.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dfms/utils.py
+++ dfms/utils.py
@@ -174,31 +174,23 @@ def getDfmsDir():
     Returns the root of the directory structure used by the DFMS framework at
     runtime.
     """
-    if 'XDG_RUNTIME_DIR' in os.environ:
-        return os.path.join(os.environ['XDG_RUNTIME_DIR'], "dfms")
     return os.path.join(os.path.expanduser("~"), ".dfms")
 
-def getDfmsPidDir(createIfMissing=False):
+def getDfmsPidDir():
     """
     Returns the location of the directory used by the DFMS framework to store
     its PIDs. If `createIfMissing` is True, the directory will be created if it
     currently doesn't exist
     """
-    path = os.path.join(getDfmsDir(), 'pid')
-    if createIfMissing:
-        createDirIfMissing(path)
-    return path
+    return os.path.join(getDfmsDir(), 'pid')
 
-def getDfmsLogsDir(createIfMissing=False):
+def getDfmsLogsDir():
     """
     Returns the location of the directory used by the DFMS framework to store
     its logs. If `createIfMissing` is True, the directory will be created if it
     currently doesn't exist
     """
-    path = os.path.join(getDfmsDir(), 'logs')
-    if createIfMissing:
-        createDirIfMissing(path)
-    return path
+    return os.path.join(getDfmsDir(), 'logs')
 
 def createDirIfMissing(path):
     """
