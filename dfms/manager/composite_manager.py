f001de666fd759603bca22eb1495fe671cc0eedc
Rodrigo Tobar <rtobar@icrar.org>
2016-04-04 12:27:07 +0800

    Cleaning up command-line calculation
    This is a bug that remains from the time when we switched from Pyro to the REST
    interface for the inter-DM communication.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dfms/manager/composite_manager.py
+++ dfms/manager/composite_manager.py
@@ -146,10 +146,7 @@ class CompositeManager(DROPManager):
         return self._dmPort
 
     def subDMCommandLine(self, host):
-        cmdline = '{0} --rest -i {1} -P {2} -d --host {3}'.format(self._dmExec, self._subDmId, self._dmPort, host)
-        if self._dmRestPort:
-            cmdline += ' --restPort {0}'.format(self._dmRestPort)
-        return cmdline
+        return '{0} -i {1} -P {2} -d --host {3}'.format(self._dmExec, self._subDmId, self._dmPort, host)
 
     def startDM(self, host):
         client = remote.createClient(host, pkeyPath=self._pkeyPath)
