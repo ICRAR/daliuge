a977372429daf6f1261d552299b805ab40f4f5d2
Rodrigo Tobar <rtobar@icrar.org>
2016-04-04 12:14:44 +0800

    New unittest that checks drop creation times
    This unit test was created while trying to duplicate a problem we were observing
    at AWS when using dfms. Drops were taking simply too long to be created, using a
    few milliseconds of CPU, with big ~2 [s] pauses in between. We wanted to make
    sure that with big graphs this isn't really the case, and at the same time to
    replicate the environment used at AWS by spawning separate processes.
    
    The test successfully runs in my machine, although I set a big threshold on the
    expected creation time just to make sure the test passes as well on slower
    hardware (but not as big as to hide the issue). In the meanwhile more clues seem
    to indicate that the problem was in the AWS infrastructure, but keeping this
    test here isn't bad anyway.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

---
+++ test/manager/test_scalability.py
@@ -0,0 +1,112 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia, 2016
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+import subprocess
+import sys
+import unittest
+
+from dfms import drop
+from dfms.manager import client
+import time
+
+
+hostname = 'localhost'
+
+
+def memory_drop(uid):
+    return drop.dropdict({'node':hostname, 'oid':uid, 'uid':uid, 'type':'plain', 'storage':'memory'})
+
+def create_graph(branches, drops_per_branch):
+    graph = []
+    completed_uids = []
+    final_apps = []
+    for branch in xrange(branches):
+        for i in xrange(drops_per_branch):
+            data_uid = 'data_%d_branch_%d' % (i, branch)
+            app_uid = 'app_%d_branch_%d' % (i, branch)
+            data_drop = memory_drop(data_uid)
+            app_drop = drop.dropdict({'node':hostname, 'oid':app_uid, 'uid':app_uid, 'type':'app', 'app':'test.graphsRepository.SleepAndCopyApp', 'sleepTime':0})
+            data_drop.addConsumer(app_drop)
+            graph.append(data_drop)
+            graph.append(app_drop)
+            if i == 0:
+                completed_uids.append(data_uid)
+                prev_app = data_drop
+            elif i == drops_per_branch - 1:
+                final_apps.append(app_drop)
+            else:
+                data_drop.addProducer(prev_app)
+
+    final_drop = memory_drop('final')
+    for final_app in final_apps:
+        final_drop.addProducer(final_app)
+
+    graph.append(final_drop)
+    return graph, completed_uids
+
+class TestBigGraph(unittest.TestCase):
+    """
+    A small class that simply checks that the deployment of a considerable-sized
+    graph takes no longer than really expected.
+    """
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+        args = [sys.executable, '-m', 'dfms.manager.cmdline', 'dfmsNM', '-H', '0.0.0.0', '-qq']
+        self.dmProcess = subprocess.Popen(args)
+
+    def tearDown(self):
+        self.dmProcess.kill()
+        unittest.TestCase.tearDown(self)
+
+    def test_submit_hugegraph(self):
+
+        sessionId = 'lala'
+        restPort  = 8888
+        args = [sys.executable, '-m', 'dfms.manager.cmdline', 'dfmsDIM', \
+                '--port', str(restPort), '-N',hostname, '-qq']
+
+        # Each branch contains a data drop and an app drop
+        # All branches connect to a final data drop
+        drops_per_branch=5000
+        branches = 5
+        n_drops = drops_per_branch * branches * 2 + 1
+        graph, completed_uids = create_graph(branches=5, drops_per_branch=drops_per_branch)
+        self.assertEquals(n_drops, len(graph))
+
+        c = client.NodeManagerClient(port=restPort)
+        dimProcess = subprocess.Popen(args)
+        try:
+            c.create_session(sessionId)
+            c.append_graph(sessionId, graph)
+
+            # What we are actually trying to measure with all this stuff
+            start = time.time()
+            c.deploy_session(sessionId, completed_uids)
+            delta = time.time() - start
+
+            # A minute is more than enough, in my PC it takes around 4 or 5 [s]
+            # A minute is also way less than the ~2 [h] we observed in AWS
+            self.assertLessEqual(delta, 60, "It took way too much time to create %d drops" % (n_drops,))
+
+        finally:
+            dimProcess.kill()
