07d910bf934b45cce79a0dc14c7fa7a1af43ff58
Chen Wu <chen.wu@icrar.org>
2016-04-14 21:54:20 +0800

    added topological comment, ready to be integrated with dfms_cluster

--- deploy/pawsey/dfms_monitor.py
+++ deploy/pawsey/dfms_monitor.py
@@ -22,13 +22,25 @@
 #
 
 """
-DFMS Monitor runs outside the Pawsey firewall (e.g. somewhere in ICRAR)
+DFMS Monitor runs outside the Pawsey firewall
+--------------------------------------------------------------------------------
+          Pawsey Magnus / Galaxy              |     Public         |
+             Private Network                  |     Network        |
+                                              |                    |
++---------+                +----------+       |      +--------+    |
+|  DFMS   |                |  DFMS    |       |      | DFMS   |    |
+| DropMgr | <== socket ==> |  Proxy   |<== socket ==>| Monitor|<- http <- Client
++---------+                +----------+       |      +--------+    |   (Browser)
+                                              |                    |
+                                           FIREWALL             GATEWAY
+                                              |                    |
+--------------------------------------------------------------------------------
 """
 
-import socket
-import select
-import time
+import socket, select
+import time, os
 import sys, logging
+from optparse import OptionParser
 
 BUFF_SIZE = 16384
 outstanding_conn = 20
@@ -36,6 +48,7 @@ default_dfms_monitor_port = 30000
 default_client_proxy_port = 30001
 MAX_TIME_OUT = 3
 MIN_TIME_OUT = 0.1
+FORMAT = "%(asctime)-15s [%(levelname)5.5s] [%(threadName)15.15s] %(name)s#%(funcName)s:%(lineno)s %(message)s"
 
 logger = logging.getLogger('deploy.pawsey.monitor')
 
@@ -45,15 +58,15 @@ class DFMSMonitor:
     monitor_sock_queue = []
     select_time_out = MAX_TIME_OUT
 
-    def __init__(self, host='0.0.0.0', dfms_port=default_dfms_monitor_port, client_port=default_client_proxy_port):
+    def __init__(self, host='0.0.0.0', monitor_port=default_dfms_monitor_port, client_port=default_client_proxy_port):
         """
         host:               listening host (string)
-        dfms_port:          monitor port exposed to the dfms drop manager  (int)
+        monitor_port:       monitor port exposed to the dfms proxy  (int)
         client_port:        proxy port exposed to the client (e.g. Firefox) (int)
         """
         self.manager_listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.monitor_listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.setup_listening_socket(self.manager_listen_socket, host, dfms_port)
+        self.setup_listening_socket(self.manager_listen_socket, host, monitor_port)
         self.setup_listening_socket(self.monitor_listen_socket, host, client_port)
 
     def setup_listening_socket(self, the_socket, host, port):
@@ -89,12 +102,11 @@ class DFMSMonitor:
                 for err_sock in exceptready:
                     self.on_close(err_sock)
 
-
     def on_accept(self, the_socket):
         clientsock, clientaddr = the_socket.accept()
         # When we receive a connection from master manager host
         if self.manager_listen_socket == the_socket:
-            print 'receiving manager_socket'
+            logger.info('receiving manager_socket')
             self.manager_socket = clientsock
             self.input_list.append(clientsock)
             if (self.monitor_socket is None and len(self.monitor_sock_queue) > 0):
@@ -117,8 +129,8 @@ class DFMSMonitor:
         # We don't have a connection to master manager yet so close the new connection
         # as we can't do anything with the data anyway!
         else:
-            print "Can't establish connection with master manager server.",
-            print "Closing connection with client side", clientaddr
+            logger.warning("Can't establish connection with master manager server.")
+            logger.info("Closing connection with client side {0}".format(clientaddr))
             self.on_close(clientsock)
 
     def on_close(self, the_socket):
@@ -154,9 +166,24 @@ class DFMSMonitor:
                     self.manager_socket.sendall(data)
 
 if __name__ == '__main__':
-    server = DFMSMonitor()
+    parser = OptionParser()
+    parser.add_option("-H", "--host", action="store", type="string",
+                    dest="host", help="The network interface the monitor is bind",
+                    default='0.0.0.0')
+    parser.add_option("-o", "--monitor_port", action="store", type="int",
+                    dest="monitor_port", help = "The monitor port exposed to the dfms proxy",
+                    default=default_dfms_monitor_port)
+    parser.add_option("-c", "--client_port", action="store", type="int",
+                    dest="client_port", help = "The proxy port exposed to the client",
+                    default=default_client_proxy_port)
+    parser.add_option("-l", "--log_dir", action="store", type="string",
+                    dest="log_dir", help="log directory for dfms monitor server", default=os.path.realpath(__file__))
+    (options, args) = parser.parse_args()
+    logfile = "{0}/dfms_monitor.log".format(os.path.dirname(options.log_dir))
+    logging.basicConfig(filename=logfile, level=logging.DEBUG, format=FORMAT)
+    server = DFMSMonitor(options.host, options.monitor_port, options.client_port)
     try:
         server.main_loop()
     except KeyboardInterrupt:
-        print "Ctrl C - Stopping server"
+        logger.warning("Ctrl C - Stopping DFMS Monitor server")
         sys.exit(1)
