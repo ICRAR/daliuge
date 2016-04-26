f1070a55ad2bd223bb8560408b6e63a279a05eaf
Rodrigo Tobar <rtobar@icrar.org>
2016-04-26 13:05:12 +0800

    Event listeners must be context managers
    So far we have been using Pyro proxies to perform remote code executions
    invisibly from within drops themselves. This is hitting us now in the face,
    because we are not properly releasing the proxy connection after performing
    some remote invocations, thus preventing the server from serving other
    requests.
    
    The only way to fix this is to properly release the connection from the proxy.
    This can be done either by explicitly calling proxy._pyroRelease() on the proxy
    object, or by using the proxy in a 'with' statement. The first option would
    polute the lower layers of the code with pyro-specific "if" statements, while
    the second requires that the drop class itself, plus a few other ones,
    implement the context management protocol, an otherwise unnecessary
    requirement. I finally chose the second option, which seems less harmful and
    more generic.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dfms/apps/dockerapp.py
+++ dfms/apps/dockerapp.py
@@ -45,7 +45,7 @@ DFMS_ROOT = '/dfms_root'
 DockerPath = collections.namedtuple('DockerPath', ('uid', 'path'))
 
 
-class ContainerIpWaiter(object):
+class ContainerIpWaiter(utils.noopctx):
     """
     A class that remembers the target DROP's uid and containerIp properties
     when its internal event has been set, and returns them when waitForIp is
