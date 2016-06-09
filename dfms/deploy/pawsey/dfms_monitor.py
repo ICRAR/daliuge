ee275f74dee172c2bcbe9b9a052447074bd8f62c
Rodrigo Tobar <rtobar@icrar.org>
2016-06-09 12:14:07 +0800

    New version of the dfms_monitor
    It opens up a new port to clients for each proxy that connects to it. When the
    proxy disconnects the client port is also closed.
    
    The code is not great, but does the work so far, or so it seems.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dfms/deploy/pawsey/dfms_monitor.py
+++ dfms/deploy/pawsey/dfms_monitor.py
@@ -20,7 +20,6 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #    MA 02111-1307  USA
 #
-
 """
 DFMS Monitor runs outside the Pawsey firewall
 --------------------------------------------------------------------------------
@@ -37,19 +36,23 @@ DFMS Monitor runs outside the Pawsey firewall
 --------------------------------------------------------------------------------
 """
 
-import socket, select, time
-import time, os, struct
-import sys, logging
-from optparse import OptionParser
-from collections import defaultdict
+import collections
+import errno
+import logging
+import optparse
+import os
+import select
+import socket
+import struct
+import sys
+import time
+
 
 BUFF_SIZE = 16384
 outstanding_conn = 20
-default_dfms_monitor_port = 30000
-default_client_proxy_port = 30001
-MAX_TIME_OUT = 3
-MIN_TIME_OUT = 0.1
-FORMAT = "%(asctime)-15s [%(levelname)5.5s] [%(threadName)15.15s] %(name)s#%(funcName)s:%(lineno)s %(message)s"
+default_proxy_port = 30000
+default_client_base_port = 30001
+FORMAT = "%(asctime)-15s [%(levelname)5.5s] %(name)s#%(funcName)s:%(lineno)s %(message)s"
 
 logger = logging.getLogger('deploy.pawsey.monitor')
 delimit = '@#%!$'
@@ -77,135 +80,302 @@ def recv_from_dfms(sock):
     length, = struct.unpack('!I', lengthbuf)
     return recvall(sock, length)
 
+sockandaddr = collections.namedtuple('sockandaddr', 'sock addr')
+
 class DFMSMonitor:
-    input_list = []
-    data = {}
-    cl_sock_dict = dict()
-    tag_dict = dict() # k - socket hash, v - socket tag
 
-    def __init__(self, host='0.0.0.0', monitor_port=default_dfms_monitor_port, client_port=default_client_proxy_port):
+    def __init__(self, host='0.0.0.0', proxy_port=default_proxy_port, client_base_port=default_client_base_port):
         """
-        host:               listening host (string)
-        monitor_port:       monitor port exposed to the dfms proxy  (int)
-        client_port:        proxy port exposed to the client (e.g. Firefox) (int)
+        host:             listening host (string)
+        proxy_port:       port exposed to the dfms proxy  (int)
+        client_base_port: base port exposed to the client (e.g. Firefox) (int)
         """
-        self.manager_listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.monitor_listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.setup_listening_socket(self.manager_listen_socket, host, monitor_port)
-        self.setup_listening_socket(self.monitor_listen_socket, host, client_port)
+        self.host = host
+        self.next_client_port = client_base_port
 
-    def make_tag(self, sock, create=True):
-        hashcode = sock.__hash__()
-        if (create):
-            tag = '{0}_{1}'.format(hashcode, time.time() - 1E9)
-            self.tag_dict[hashcode] = tag
-        else:
-            tag = self.tag_dict.get(hashcode, None)
+        # All our sets of sockets:
+        # * One always listening for all incoming proxy connections
+        # * The list of currently opened proxy connections (idx by tag)
+        # * Many listening for incoming client connections, one per proxy connection
+        # * Many currently opened client connection (idx by port)
+        self.proxy_listener_socket = None
+        self.proxy_sockets = {}
+        self.client_listener_sockets = {}
+        self.client_sockets = {}
+
+        # Mapping between proxy socket port numbers and client listening ports
+        # This is used to route client requests through the correct proxy socket
+        self.client_port_to_proxy_port = {}
+
+        # To save the tags we attach to each client socket
+        self.tag_dict = {} # k - socket hash, v - socket tag
+
+        # Set up the single socket that listens for proxy connection
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((host, proxy_port))
+        sock.listen(outstanding_conn)
+        self.proxy_listener_socket = sock
+        self.ifds = [self.proxy_listener_socket]
+        logger.info('Listening for proxies at %s:%d', self.host, proxy_port)
+
+    def tag_for_socket(self, sock, create=True):
+        hashcode = hash(sock)
+        if not create:
+            return self.tag_dict[hashcode]
+
+        tag = '{0}_{1}'.format(hashcode, time.time() - 1E9)
+        self.tag_dict[hashcode] = tag
         return tag
 
-    def setup_listening_socket(self, the_socket, host, port):
-        the_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        the_socket.bind((host, port))
-        the_socket.listen(outstanding_conn)
+    def ioloop(self):
+        while True:
 
-    def main_loop(self):
-        self.input_list.append(self.manager_listen_socket)
-        self.input_list.append(self.monitor_listen_socket)
-        self.manager_socket = None
-        count = 0
-        remove_dict = defaultdict(int)
-        while 1:
-            inputready, outputready, exceptready = select.select(self.input_list + self.cl_sock_dict.values(), [], [])
-            for index, the_socket in enumerate(inputready):
-                if the_socket == self.manager_listen_socket or\
-                   the_socket == self.monitor_listen_socket:
-                    self.on_accept(the_socket)
-                else:
-                    if (the_socket == self.manager_socket):
-                        data = recv_from_dfms(the_socket)
-                        if (data is None):
-                            logger.warning("Socket to dfms proxy is broken")
-                            self.input_list.remove(the_socket)
-                            if (self.manager_socket):
-                                self.manager_socket.close()
-                                self.manager_socket = None
-                            continue
-                        at = data.find(delimit)
-                        if (at == -1):
-                            logger.error('No tag id from DFMS proxy, discard the message')
-                            continue
-                        else:
-                            tag = data[0:at]
-                        logger.debug("Received {0} from DFMS proxy".format(tag))
-                        client_sock = self.cl_sock_dict.get(tag, None)
-                        if (client_sock is None):
-                            pass
-                        else:
-                            to_send = data[at + dl:]
-                            leng = len(to_send)
-                            client_sock.sendall(to_send)
-                            logger.debug("Sent {0} to Client".format(tag))
-                    else: # from one of the client sockets
-                        data = the_socket.recv(BUFF_SIZE)
-                        tag = self.make_tag(the_socket, create=False)
-                        logger.debug("Received {0} from Client".format(tag))
-                        send_to_dfms(self.manager_socket, delimit.join([tag, data]))
-                        logger.debug("Sent {0} to DFMS proxy".format(tag))
-                        if (len(data) == 0):
-                            del self.cl_sock_dict[tag]
-                            del self.tag_dict[the_socket.__hash__()]
-                            the_socket.close()
+            try:
+                inputready, _, _ = select.select(self.ifds, [], [])
 
-    def on_accept(self, the_socket):
-        clientsock, clientaddr = the_socket.accept()
-        if self.manager_listen_socket == the_socket:
-            logger.info('receiving new socket from dfms_proxy')
-            if (self.manager_socket is not None):
-                raise Exception("already not none!")
-            self.manager_socket = clientsock
-            self.input_list.append(self.manager_socket)
-        # else when we receive a connection from a localhost
-        # AND we have a connection to the master maanger
-        elif self.manager_socket is not None:
-            tag = self.make_tag(clientsock)
-            #logger.info('receiving new socket {0} from client'.format(tag))
-            if (self.cl_sock_dict.has_key(tag)):
-                raise Exception("duplicated tag {0}".format(tag))
-            self.cl_sock_dict[str(tag)] = clientsock
-            logger.debug("Created Client socket {0}".format(tag))
-            #self.input_list.append(clientsock)
-        else:
-            logger.debug('receiving socket from client, but')
-            logger.warning("Can't establish connection with master manager server.")
-            logger.info("Closing connection with client side {0}".format(clientaddr))
+                # The self.* lists are continuously updated by the on_* methods,
+                # so we keep a reference to the initial values they have.
+                # This means that the methods must be prepared to accept a socket
+                # that is not really working anymore
+                proxy_sockets = [x.sock for x in self.proxy_sockets.values()]
+                client_sockets = [x.sock for x in self.client_sockets.values()]
+
+                for sock in inputready:
+                    if sock == self.proxy_listener_socket:
+                        self.on_proxy_connected(sock)
+                    elif sock in self.client_listener_sockets.values():
+                        self.on_client_connected(sock)
+                    elif sock in proxy_sockets:
+                        self.on_proxy_data(sock)
+                    elif sock in client_sockets:
+                        self.on_client_data(sock)
+                    else:
+                        logger.error("Received data from unknown socket: %r", sock)
+            except KeyboardInterrupt:
+                raise
+            except Exception:
+                logger.exception("Unexpected exception, some communications might have been lost")
+
+    def add_client_listener(self):
+
+        client_listener_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_listener_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        bound = False
+        port = self.next_client_port
+        while not bound:
+            try:
+                client_listener_socket.bind((self.host, port))
+                self.next_client_port += 1
+                logger.info('Listening for clients at %s:%d', self.host, port)
+                bound = True
+            except socket.error as e:
+                if e.errno != errno.EADDRINUSE:
+                    raise
+                logger.info("Port %d already bound, trying next", port)
+                port += 1
+
+        client_listener_socket.listen(outstanding_conn)
+        return client_listener_socket
+
+    def close_socket(self, sock, shutdown=True):
+
+        # Maybe we already got rid of it...
+        if sock not in self.ifds:
+            return
+
+        # We don't listen to it anymore, remove it from our records
+        self.ifds.remove(sock)
+
+        if shutdown:
+            try:
+                sock.shutdown(socket.SHUT_RDWR)
+            except socket.error:
+                logger.exception("Error while shutting down socket %r, continuing anyway", sock.getsockname())
+
+        try:
+            sock.close()
+        except socket.error:
+            logger.exception("Error while closing socket %r, continuing anyway", sock.getsockname())
+
+    def remove_client_socket(self, sock):
+        tag = self.tag_for_socket(sock, create=False)
+        client_sockandaddr = self.client_sockets[tag]
+        logger.info("Closing client socket %r (%s)", client_sockandaddr.addr, tag)
+        self.close_socket(sock, False)
+        del self.client_sockets[tag]
+        del self.tag_dict[hash(sock)]
+
+    def remove_proxy_socket(self, sock):
+
+        for proxyport,saa in self.proxy_sockets.items():
+            if saa.sock == sock:
+                break
+
+        # Close the proxy socket itself
+        logger.info("Closing proxy socket %r", saa.addr)
+        del self.proxy_sockets[proxyport]
+        self.close_socket(sock, False)
+
+        # Close the client listener port associated to this proxy
+        # so no more incoming client requests are received
+        clientport = None
+        for clientport, pp in self.client_port_to_proxy_port.items():
+            if pp == proxyport:
+                break
+        if clientport is None:
+            raise Exception("This shouldn't have happened, sorry :-(")
+
+        self.remove_clientlistener_socket(clientport)
+
+        clisocksandaddr = self.client_sockets.values()
+        for saa in clisocksandaddr:
+            this_clientport = saa.addr[1]
+            if this_clientport == clientport:
+                self.close_socket(saa.sock)
+
+    def remove_clientlistener_socket(self, port):
+        cls = self.client_listener_sockets[port]
+        logger.info("Closing client listener socket %r", cls.getsockname())
+        self.close_socket(cls)
+        del self.client_listener_sockets[port]
+        del self.client_port_to_proxy_port[port]
+        if port < self.next_client_port:
+            self.next_client_port = port
+
+    def on_proxy_connected(self, sock):
+
+        proxysock, proxyaddr = sock.accept()
+        proxyport = proxyaddr[1]
+        self.proxy_sockets[proxyport] = sockandaddr(proxysock, proxyaddr)
+        self.ifds.append(proxysock)
+        logger.info('Received new connection from dfms_proxy at %r', proxyaddr)
+
+        client_listener_socket = self.add_client_listener()
+        clientport = client_listener_socket.getsockname()[1]
+        self.client_listener_sockets[clientport] = client_listener_socket
+        self.client_port_to_proxy_port[clientport] = proxyport
+        self.ifds.append(client_listener_socket)
+
+    def on_client_connected(self, sock):
+
+        clientsock, clientaddr = sock.accept()
+
+        if len(self.proxy_sockets) == 0:
+            # This shouldn't happen though...
+            logger.debug("Received client connection, but no proxy connections ready yet, ignoring")
+            clientsock.shutdown()
             clientsock.close()
+            return
+
+        # Unique per-client-connection tag
+        tag = self.tag_for_socket(clientsock)
+        if tag in self.client_sockets:
+            raise Exception("Duplicated tag {0}".format(tag))
+        self.client_sockets[tag] = sockandaddr(clientsock, sock.getsockname())
+
+        # Check for incoming data
+        self.ifds.append(clientsock)
+        logger.info('Received new client connection %r -> %s (%s)', clientaddr, sock.getsockname(), tag)
+
+    def on_proxy_data(self, sock):
+
+        try:
+            data = recv_from_dfms(sock)
+        except socket.error:
+            logger.warning("Error while reading data from proxy, will close it")
+            self.remove_proxy_socket(sock)
+            return
+
+        if data is None:
+            logger.warning("Proxy disconnected")
+            self.remove_proxy_socket(sock)
+            return
+
+        at = data.find(delimit)
+        if at == -1:
+            logger.error('No tag id from DFMS proxy, discard the message')
+            return
+
+        tag = data[0:at]
+        logger.debug("Received %s from DFMS proxy", tag)
+
+        if tag not in self.client_sockets:
+            logger.warning("Client %s has already disconnected, discarding data from proxy", tag)
+            return
+
+        client_sockandaddr = self.client_sockets[tag]
+        if client_sockandaddr is None:
+            logger.warning("Couldn't find client for tag '%s' of proxy %r", tag, sock.getsockname())
+            return
+        client_sock = client_sockandaddr.sock
+
+        to_send = data[at + dl:]
+        try:
+            client_sock.sendall(to_send)
+            logger.debug("Sent data to client %s", tag)
+        except socket.error:
+            logger.warning("Error while writing to client %r, we'll probably detect it later", client_sockandaddr.addr)
+
+    def on_client_data(self, sock):
+
+        tag = self.tag_for_socket(sock, create=False)
+        data = sock.recv(BUFF_SIZE)
+
+        # The client disconnected, remove it
+        if not data:
+            logger.info("Client %s disconnected", tag)
+            self.remove_client_socket(sock)
+            return
+
+        logger.debug("Received data from client %s", tag)
+        proxy_port = self.client_port_to_proxy_port[sock.getsockname()[1]]
+        proxy_socket = None
+        for port,proxy_sock in self.proxy_sockets.items():
+            if port == proxy_port:
+                proxy_socket = proxy_sock.sock
+                break
+
+        if proxy_socket is None:
+            raise Exception("shouldn't happen, right?")
+
+        try:
+            send_to_dfms(proxy_socket, delimit.join([tag, data]))
+            logger.debug("Sent data from client %s to proxy", tag)
+        except socket.error:
+            logger.warning("Error while sending data to proxy, closing proxy connection")
+            self.close_socket(proxy_socket)
 
 if __name__ == '__main__':
-    parser = OptionParser()
+
+    parser = optparse.OptionParser()
     parser.add_option("-H", "--host", action="store", type="string",
                     dest="host", help="The network interface the monitor is bind",
                     default='0.0.0.0')
     parser.add_option("-o", "--monitor_port", action="store", type="int",
                     dest="monitor_port", help = "The monitor port exposed to the dfms proxy",
-                    default=default_dfms_monitor_port)
+                    default=default_proxy_port)
     parser.add_option("-c", "--client_port", action="store", type="int",
                     dest="client_port", help = "The proxy port exposed to the client",
-                    default=default_client_proxy_port)
+                    default=default_client_base_port)
     parser.add_option("-l", "--log_dir", action="store", type="string",
                     dest="log_dir", help="log directory for dfms monitor server", default=os.path.realpath(__file__))
     parser.add_option("-d", "--debug",
                   action="store_true", dest="debug", default=False,
                   help="Whether to log debug info")
     (options, args) = parser.parse_args()
+
     logfile = "{0}/dfms_monitor.log".format(os.path.dirname(options.log_dir))
     if (options.debug):
         ll = logging.DEBUG
     else:
         ll = logging.INFO
-    logging.basicConfig(filename=logfile, level=ll, format=FORMAT)
+    logging.basicConfig(stream=sys.stdout, level=ll, format=FORMAT)
+
     server = DFMSMonitor(options.host, options.monitor_port, options.client_port)
     try:
-        server.main_loop()
+        server.ioloop()
     except KeyboardInterrupt:
         logger.warning("Ctrl C - Stopping DFMS Monitor server")
         sys.exit(1)
