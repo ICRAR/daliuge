9e5f2733faa0310e79d0a44a8e16bc9e3d580f01
Rodrigo Tobar <rtobar@icrar.org>
2016-07-19 10:59:49 +0800

    Reverting back to Pyro's pickle serializer
    The serpent serializer started to fail with python3 (maybe because of the
    bytes/bytearray types, but not fully confirmed). Reverting back to pickle fixes
    the issue and works both for python 2 and 3, while at the same time brings
    better performance. The old serpent setup code is still available in case we
    want to use it again.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dfms/__init__.py
+++ dfms/__init__.py
@@ -28,8 +28,7 @@ import logging
 
 def _setupPyro():
     """
-    Sets up Pyro to add support for sending Event objects through and to set a
-    finite default call timeout.
+    Sets up Pyro configuration items.
 
     Pyro >= 4.20 uses the 'serpent' serializer by default. In this serializer
     "most custom classes aren't dealt with automatically" [1], including our
@@ -37,11 +36,14 @@ def _setupPyro():
     either instruct Pyro how to serialize the Event class, or to use the
     'pickle' serializer.
 
-    We currently chose to add explicit support for the Event class and keep
-    using the 'serpent' serializer. Although more complex, it should be safer
-    (see [1] again). We left commented out the lines that would instruct Pyro to
-    use the 'pickle' serializer instead, in which case the custom converter
-    registration isn't needed.
+    We used to choose to add explicit support for the Event class and keep
+    using the 'serpent' serializer. Although more complex, it's in theory safer
+    (see [1] again). Once we started supporting python 3 we found that the
+    serpent serializer wasn't working correctly, most probably (but not totally
+    proven) having troubles with the bytes/bytearray data types. This forced us
+    to move back to the pickle serializer, which seems to perform better anyway.
+    We leave the previous serpent-based configuration as a reference in case we
+    want to revert to it.
 
     In Pyro >= 4.46 the REQUIRE_EXPOSE configuration flag was defaulted to True.
     Instead of embracing it (which would require us to change all our drop
@@ -50,32 +52,38 @@ def _setupPyro():
     [1] https://pythonhosted.org/Pyro4/clientcode.html#serialization
     """
 
-    import importlib
     import Pyro4
-    from dfms.event import Event
 
-    def __pyro4_class_to_dict(o):
-        d = {'__class__' : o.__class__.__name__, '__module__': o.__class__.__module__}
-        d.update(o.__dict__)
-        return d
+    def setup_serpent():
 
-    def __pyro4_dict_to_class(classname, d):
-        modname = d['__module__']
-        module = importlib.import_module(modname)
-        clazz = getattr(module, classname)
-        o = clazz()
-        for k in d:
-            if k in ['__class__', '__module__']: continue
-            setattr(o, k, d[k])
-        return o
+        from dfms.event import Event
 
-    Pyro4.util.SerializerBase.register_class_to_dict(Event, __pyro4_class_to_dict)
-    Pyro4.util.SerializerBase.register_dict_to_class('Event', __pyro4_dict_to_class)
+        def __pyro4_class_to_dict(o):
+            d = {'__class__' : o.__class__.__name__, '__module__': o.__class__.__module__}
+            d.update(o.__dict__)
+            return d
 
-    # This is another way of setting up Pyro to let it serialize the Event
-    # class properly: using the pickle serializer
-    #Pyro4.config.SERIALIZER = 'pickle'
-    #Pyro4.config.SERIALIZERS_ACCEPTED = ['pickle']
+        def __pyro4_dict_to_class(classname, d):
+            import importlib
+            modname = d['__module__']
+            module = importlib.import_module(modname)
+            clazz = getattr(module, classname)
+            o = clazz()
+            for k in d:
+                if k in ['__class__', '__module__']: continue
+                setattr(o, k, d[k])
+            return o
+
+        Pyro4.util.SerializerBase.register_class_to_dict(Event, __pyro4_class_to_dict)
+        Pyro4.util.SerializerBase.register_dict_to_class('Event', __pyro4_dict_to_class)
+
+    def setup_pickle():
+        Pyro4.config.SERIALIZER = 'pickle'
+        Pyro4.config.SERIALIZERS_ACCEPTED = ['pickle']
+
+    # We could also do one or the other depending on the major version of python
+    #setup_serpent()
+    setup_pickle()
 
     # In Pyro4 >= 4.46 the default for this option changed to True, which would
     # mean we need to decorate all our classes with Pyro-specific code.
