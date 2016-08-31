bfac27cfaf95d5a4bc9007db6eb421a09e0038c0
Rodrigo Tobar <rtobar@icrar.org>
2016-08-31 16:16:42 +0800

    Removing unused import
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dfms/manager/client.py
+++ dfms/manager/client.py
@@ -19,7 +19,6 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #    MA 02111-1307  USA
 #
-import json
 import logging
 
 from six.moves import urllib_parse as urllib  # @UnresolvedImport
