15f74eb84f21c684d74096707400a6cd36310c35
Chen Wu <chen.wu@icrar.org>
2016-09-20 22:33:44 +0800

    fix the KeyError when mysarkar has only 1 island, use form_island as the condition

--- dfms/dropmake/pg_generator.py
+++ dfms/dropmake/pg_generator.py
@@ -674,7 +674,6 @@ class PGT(object):
         nm_list = node_list[num_islands:]
         nm_len = len(nm_list)
 
-        no_merge = False
         if (form_island):
             self.merge_partitions(num_islands, form_island=True)
             # from Eq.1 we know that num_parts <= nm_len
@@ -682,8 +681,6 @@ class PGT(object):
         elif (nm_len < num_parts):
             self.merge_partitions(nm_len, form_island=False)
             num_parts = nm_len
-        else:
-            no_merge = True
 
         lm = self._oid_gid_map
         lm2 = self._gid_island_id_map
@@ -694,7 +691,7 @@ class PGT(object):
             #TODO consider distance between a pair of nodes
             gid = lm[oid] % num_parts
             drop['node'] = nm_list[gid]
-            isid = 0 if no_merge else lm2[gid] % num_islands
+            isid = lm2[gid] % num_islands if form_island else 0
             drop['island'] = is_list[isid]
 
         if (ret_str):
