5eed9d3b82484f08984317c2e878508d27fdf2d1
Chen Wu <chen.wu@icrar.org>
2016-09-27 17:50:15 +0800

    add import statements

--- dfms/deploy/pawsey/min_test.py
+++ dfms/deploy/pawsey/min_test.py
@@ -20,13 +20,75 @@
 #    MA 02111-1307  USA
 #
 
+"""
+Pre-test the platform compatibility for deploying the DALiuGE cluster
+"""
+
 import commands, time, sys, os, logging
 import json
 from optparse import OptionParser
 import threading
 from collections import defaultdict
 
+from mpi4py import MPI
+
 logger = logging.getLogger('deploy.pawsey.min_test')
+
+def import_node_mgr():
+    from dfms import utils
+    from dfms.drop import AppDROP
+    from dfms.exceptions import NoSessionException, SessionAlreadyExistsException,\
+        DaliugeException
+    from dfms.lifecycle.dlm import DataLifecycleManager
+    from dfms.manager import repository, constants
+    from dfms.manager.drop_manager import DROPManager
+    from dfms.manager.session import Session
+
+def import_start_cluster():
+    import dfms.deploy.pawsey.dfms_proxy as dfms_proxy
+    from dfms.deploy.pawsey.example_client import MonitorClient
+    import dfms.deploy.pawsey.example_client as exclient
+    from dfms.manager.client import DataIslandManagerClient
+    import dfms.manager.cmdline as dfms_start
+    from dfms.manager.constants import NODE_DEFAULT_REST_PORT, \
+    ISLAND_DEFAULT_REST_PORT, MASTER_DEFAULT_REST_PORT
+
+def import_composite_mgr():
+    import abc
+    import collections
+    import functools
+    import logging
+    import multiprocessing.pool
+    import threading
+
+    from dfms import remote, graph_loader
+    from dfms.ddap_protocol import DROPRel
+    from dfms.exceptions import InvalidGraphException, DaliugeException, \
+        SubManagerException
+    from dfms.manager.client import NodeManagerClient
+    from dfms.manager.constants import ISLAND_DEFAULT_REST_PORT, NODE_DEFAULT_REST_PORT
+    from dfms.manager.drop_manager import DROPManager
+    from dfms.utils import portIsOpen
+    from dfms.manager import constants
+
+def import_cmdline():
+    from dfms.manager.composite_manager import DataIslandManager, MasterManager
+    from dfms.manager.constants import NODE_DEFAULT_REST_PORT, \
+        ISLAND_DEFAULT_REST_PORT, MASTER_DEFAULT_REST_PORT, REPLAY_DEFAULT_REST_PORT
+    from dfms.manager.node_manager import NodeManager
+    from dfms.manager.replay import ReplayManager, ReplayManagerServer
+    from dfms.manager.rest import NMRestServer, CompositeManagerRestServer, \
+        MasterManagerRestServer
+    from dfms.utils import getDfmsPidDir, getDfmsLogsDir, createDirIfMissing
+
+def import_rest():
+    from dfms.exceptions import InvalidGraphException, InvalidSessionState, \
+        DaliugeException, NoSessionException, SessionAlreadyExistsException, \
+        InvalidDropException, InvalidRelationshipException
+    from dfms.manager import constants
+    from dfms.manager.client import NodeManagerClient
+    from dfms.restutils import RestServer, RestClient, DALIUGE_HDR_ERR, \
+        RestClientException
 
 def get_ip(loc='Pawsey'):
     """
@@ -54,11 +116,22 @@ if __name__ == '__main__':
     parser.add_option("-i", "--import_dfms", action="store_true",
                       dest="import_dfms", help="Whether to import DFMS libraries", default=False)
 
+    parser.add_option("-l", "--log_to_file", action="store_true",
+                      dest="log_to_file", help="Whether to log onto files", default=False)
+
     (options, args) = parser.parse_args()
 
-    comm = MPI.COMM_WORLD  # @UndefinedVariable
+    comm = MPI.COMM_WORLD
     num_procs = comm.Get_size()
     rank = comm.Get_rank()
+
+    FORMAT = "%(asctime)-15s [%(levelname)5.5s] [%(threadName)15.15s] %(name)s#%(funcName)s:%(lineno)s %(message)s"
+    if (options.log_to_file):
+        log_dir = '/group/astronomy856/cwu/dfms/logs/min_test'
+        logfile = "{0}/{1}.log".format(log_dir, rank)
+        logging.basicConfig(filename=logfile, level=logging.DEBUG, format=FORMAT)
+    else:
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=FORMAT)
 
     public_ip = get_ip()
     ip_adds = public_ip
@@ -66,19 +139,17 @@ if __name__ == '__main__':
 
     if (rank == 0):
         logger.info("manager ip: {0}".format(public_ip))
-        logger.info("got a list of IPs: {1}".format(ip_adds))
+        logger.info("got a list of IPs: {0}".format(ip_adds))
     else:
-        logger.info("my ip: {0}".format(public_ip))
+        logger.info("my ip: {0}, my rank: {1}".format(public_ip, rank))
 
     if (options.import_dfms):
         logger.info("importing a bunch of DFMS libraries")
+        import_start_cluster()
+        import_cmdline()
         if (rank == 0):
-            import dfms.deploy.pawsey.dfms_proxy as dfms_proxy
-            from dfms.deploy.pawsey.example_client import MonitorClient
-            import dfms.deploy.pawsey.example_client as exclient
+            import_composite_mgr()
         else:
-            from dfms.manager.client import DataIslandManagerClient
-            import dfms.manager.cmdline as dfms_start
-            from dfms.manager.constants import NODE_DEFAULT_REST_PORT, \
-            ISLAND_DEFAULT_REST_PORT, MASTER_DEFAULT_REST_PORT
+            import_node_mgr()
+        import_rest()
         logger.info("Importing successful")
