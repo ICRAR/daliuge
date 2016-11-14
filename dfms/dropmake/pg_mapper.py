b6d336c8f667627925cce5c2e04b6d18502a3974
Chen Wu <chen.wu@icrar.org>
2016-11-14 13:00:53 +0800

    add resource capability class

--- dfms/dropmake/pg_mapper.py
+++ dfms/dropmake/pg_mapper.py
@@ -34,20 +34,21 @@ clustering-based algorithms (e.g. MetisPGTP, MySarkarPGTP) that have two issues:
    More importantly, such partial graph deployment is currently not supported
    unless we also change DropManager's behaviours in receiving PGs.
 
-2. The complexity of maintaining te DoP constraint within each partition is too
+2. The complexity of maintaining the DoP constraint within each partition is too
    high as the # of Drops goes up (exponential), and will only get worse after
    adding more constraints such as CPUs or memory consumption
-   The key issue here is that we tried to use graph
-   theory (antichains) to prevent something bad from hapening in the future. This
+   The key issue here is that we tried to use complex graph models (antichains)
+   to prevent something bad from hapening in the future. This
    in turn requires significant computation (e.g. complete transitive closures).
-   Instead, we could just "go" to the future, disallow bad things via simulations
-   by playing out the HEFT algorithm
+   Instead, we could just "go" to the future, disallow bad things via
+   simulations by playing out the mapping algorithm step by step
 
 [1] Deelman, Ewa, et al. "Pegasus: A framework for mapping complex scientific
 workflows onto distributed systems." Scientific Programming 13.3 (2005): 219-237.
 
 In addition, for there is a more practical issue associated with the most
-popular METIS algorithm --- cannot minimise completion time
+popular METIS algorithm --- cannot minimise completion time, does not really
+support DAG
 
 The mapping algorithm will be based on the HEFT algorithm
 http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=993206
@@ -69,12 +70,15 @@ class ResourceCapability(object):
                 num_cores_per_node, intra_inter_ratio):
         """
         intra_inter_ratio:  ratio between intra-island bandwith and inter-island
-                            bandwidth (integer and >= 1)
+                            bandwidth (integer and should be >= 1)
         """
+        if (intra_inter_ratio < 1):
+            raise GraphException('Invalid intra_inter_ratio {0}'\
+            .format(intra_inter_ratio))
         self._num_island = num_island
         self._num_nodes_pi = num_nodes_per_island
         self._num_cores_pn = num_cores_per_node # per compute node
-        self._island_ratio = intra_inter_ratio
+        self._island_ratio = float(intra_inter_ratio)
         self._ttnodes = num_island * num_nodes_per_island
 
     def bandwidth(self, nodeA, nodeB):
@@ -84,19 +88,23 @@ class ResourceCapability(object):
         if (nodeA >= self._ttnodes or nodeB >= self._ttnodes):
             raise GraphException('Invalid node id')
         gap = nodeA // self._num_nodes_pi - nodeB // self._num_nodes_pi
-        return intra_inter_ratio if gap == 0 else 1
+        return self._island_ratio  if gap == 0 else 1.0
 
-    def comp(self, drop, comp_node):
+    def avg_comp_cost(self, drop, comp_node):
         """
-        Comput cost in time
+        Comput cost in time, assuming homogeneity for now
         """
-        return drop['tw']
+        return float(drop['tw'])
 
-    def comm(self, drop_edge_weight, nodeA, nodeB):
+    def comm_cost(self, drop_edge_weight, nodeA, nodeB):
         """
         Communication cost in time
         """
-        return drop_edge_weight / float(self.bandwidth(nodeA, nodeB))
+        return drop_edge_weight / self.bandwidth(nodeA, nodeB)
+
+    def avg_comm_cost(self, drop_edge_weight):
+        return (drop_edge_weight / 1.0 +
+                drop_edge_weight / self._island_ratio) / 2
 
 class ResourceAvailability(ResourceCapability):
     pass
