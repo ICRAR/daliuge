dd1b07c0e40e0e945ea558dada58413067bde968
Rodrigo Tobar <rtobar@icrar.org>
2016-11-22 14:21:55 +0800

    General cleanup of test_scheduler.
    There were many commented statements and unused variables that made the tests
    unreadable. Now it's much easier to follow what's going on, even if the tests
    maybe don't make sense...
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- test/dropmake/test_scheduler.py
+++ test/dropmake/test_scheduler.py
@@ -43,123 +43,81 @@ class TestScheduler(unittest.TestCase):
     def test_incremental_antichain(self):
         part = Partition(100, 8)
         G = part._dag
-        #G.add_edge(1, 2)
         assert(part.probe_max_dop(1, 2, True, True, True) == DAGUtil.get_max_dop(part._dag))
-        #print "-----"
         G.add_edge(2, 3)
         assert(part.probe_max_dop(2, 3, False, True, True) == DAGUtil.get_max_dop(part._dag))
-        #print "-----"
         G.add_edge(1, 4)
         assert(part.probe_max_dop(1, 4, False, True, True) == DAGUtil.get_max_dop(part._dag))
-        #print "-----"
         G.add_edge(2, 5)
         l = part.probe_max_dop(2, 5, False, True, True)
         r = DAGUtil.get_max_dop(part._dag)
-        #print l, r
         assert l == r, "l = {0}, r = {1}".format(l, r)
 
     def test_basic_scheduler(self):
         fp = get_lg_fname('lofar_std.json')
         lg = LG(fp)
         drop_list = lg.unroll_to_tpl()
-        mys = Scheduler(drop_list)
-        #print mys._dag.edges(data=True)
+        Scheduler(drop_list)
 
     def test_minnumparts_scheduler(self):
-        lgnames = ['cont_img.json', 'lofar_std.json', 'test_grpby_gather.json', 'chiles_simple.json']
-        tgt_deadline = [500, 200, 300, 90, 80, 160] #250
+        lgs = {'cont_img.json': 500, 'lofar_std.json': 200, 'test_grpby_gather.json': 90, 'chiles_simple.json': 160}
         mdp = 8
         ofa = 0.5
-        for j, lgn in enumerate(lgnames):
+        for lgn, deadline in lgs.items():
             fp = get_lg_fname(lgn)
             lg = LG(fp)
             drop_list = lg.unroll_to_tpl()
-            #logger.info("MinNumPartsScheduler Partitioning ", lgn)
-            lll = len(lgn) + len("Partitioning ") + 1
-            #logger.info("=" * lll)
-            mps = MinNumPartsScheduler(drop_list, tgt_deadline[j], max_dop=mdp, optimistic_factor=ofa)
-            num_parts_done, lpl, ptime, parts = mps.partition_dag()
-            #logger.info("{3} partitioned: parts = {0}, lpl = {1}, ptime = {2:.2f}".format(num_parts_done, lpl, ptime, lgn))
-            #logger.info("-" * lll)
+            mps = MinNumPartsScheduler(drop_list, deadline, max_dop=mdp, optimistic_factor=ofa)
+            mps.partition_dag()
 
     def test_mysarkar_scheduler(self):
-        lgnames = ['cont_img.json', 'lofar_std.json', 'test_grpby_gather.json', 'chiles_simple.json']
-        #lgnames = [lgnames[1]]
-        tgt_partnum = [20, 15, 15, 10, 10, 5]
+        lgs = {'cont_img.json': 20, 'lofar_std.json': 15, 'test_grpby_gather.json': 10, 'chiles_simple.json': 5}
         mdp = 8
-        s_matrix = True
-        for j, lgn in enumerate(lgnames):
+        for lgn, numparts in lgs.items():
             fp = get_lg_fname(lgn)
             lg = LG(fp)
             drop_list = lg.unroll_to_tpl()
-            #logger.info( "MySarkarScheduler Partitioning ", lgn)
-            lll = len(lgn) + len("Partitioning ") + 1
-            #logger.info( "=" * lll)
             mys = MySarkarScheduler(drop_list, max_dop=mdp)
-            num_parts_done, lpl, ptime, parts = mys.partition_dag()
-            #logger.info( "{3} partitioned: parts = {0}, lpl = {1}, ptime = {2:.2f}".format(num_parts_done, lpl, ptime, lgn))
-            if (s_matrix):
-                for i, part in enumerate(parts):
-                    if (part.cardinality > 0):
-                        ma = part.schedule.schedule_matrix
-                        ga = DAGUtil.ganttchart_matrix(part.schedule._dag, part.schedule._topo_sort)
-                        # print "Partition ", i
-                        # print "scheduling matrix: ", ma.shape
-                        # print ma
-                        # print "ganttchart matrix: ", ga.shape
-                        # print ga
-                        # print "Workload: ", part.schedule.workload
-                        # print
-            mys.merge_partitions(tgt_partnum[j])
-            #logger.info( "-" * lll)
+            _, _, _, parts = mys.partition_dag()
+            for part in parts:
+                if (part.cardinality > 0):
+                    part.schedule.schedule_matrix
+                    DAGUtil.ganttchart_matrix(part.schedule._dag, part.schedule._topo_sort)
+            mys.merge_partitions(numparts)
 
     @unittest.skipIf(skip_long_tests, "Skipping because they take too long. Chen to eventually shorten them")
     def test_pso_scheduler(self):
-        lgnames = ['cont_img.json', 'lofar_std.json', 'test_grpby_gather.json', 'chiles_simple.json']
-        #lgnames = ['test_seq_gather.json']
-        tgt_deadline = [540, 450, 60, 70, 60, 160, 150] #250
-        #tgt_deadline = [150]
+        lgs = {'cont_img.json': 540, 'lofar_std.json': 450, 'test_grpby_gather.json': 70, 'chiles_simple.json': 160}
         mdp = 2
-        for j, lgn in enumerate(lgnames):
+        for lgn, deadline in lgs.items():
             fp = get_lg_fname(lgn)
             lg = LG(fp)
             drop_list = lg.unroll_to_tpl()
             psps01 = PSOScheduler(drop_list, max_dop=mdp)
-            num_parts_done, lpl, ptime, parts = psps01.partition_dag()
-            #print "PSO (no deadline): {3} partitioned: parts = {0}, lpl = {1}, ptime = {2:.2f}".format(num_parts_done, lpl, ptime, lgn)
-            psps02 = PSOScheduler(drop_list, max_dop=mdp, deadline=tgt_deadline[j])
-            num_parts_done, lpl, ptime, parts = psps02.partition_dag()
-            #print "PSO (deadline): {3} partitioned: parts = {0}, lpl = {1}, deadline = {4}, ptime = {2:.2f}".format(num_parts_done, lpl, ptime, lgn, tgt_deadline[j])
+            psps01.partition_dag()
+            psps02 = PSOScheduler(drop_list, max_dop=mdp, deadline=deadline)
+            psps02.partition_dag()
 
     @unittest.skipIf(skip_long_tests, "Skipping because they take too long. Chen to eventually shorten them")
     def test_sa_scheduler(self):
-        lgnames = ['lofar_std.json']
-        tgt_deadline = [450]
+        lgs = {'lofar_std.json': 450}
         mdp = 4
-        for j, lgn in enumerate(lgnames):
+        for lgn, deadline in lgs.items():
             fp = get_lg_fname(lgn)
             lg = LG(fp)
             drop_list = lg.unroll_to_tpl()
             pssa01 = SAScheduler(drop_list, max_dop=mdp)
-            #pssa01 = PSOScheduler(drop_list, max_dop=mdp)
-            num_parts_done, lpl, ptime, parts = pssa01.partition_dag()
-            #print "SA (no deadline): {3} partitioned: parts = {0}, lpl = {1}, ptime = {2:.2f}".format(num_parts_done, lpl, ptime, lgn)
-            pssa02 = SAScheduler(drop_list, max_dop=mdp, deadline=tgt_deadline[j])
-            num_parts_done, lpl, ptime, parts = pssa02.partition_dag()
-            #print "SA (deadline): {3} partitioned: parts = {0}, lpl = {1}, deadline = {4}, ptime = {2:.2f}".format(num_parts_done, lpl, ptime, lgn, tgt_deadline[j])
+            pssa01.partition_dag()
+            pssa02 = SAScheduler(drop_list, max_dop=mdp, deadline=deadline)
+            pssa02.partition_dag()
 
     @unittest.skipIf(skip_long_tests, "Skipping because they take too long. Chen to eventually shorten them")
     def test_mcts_scheduler(self):
-        lgnames = ['lofar_std.json']
-        tgt_deadline = [450]
+        lgs = {'lofar_std.json': 450}
         mdp = 4
-        for j, lgn in enumerate(lgnames):
+        for lgn, deadline in lgs.items():
             fp = get_lg_fname(lgn)
             lg = LG(fp)
             drop_list = lg.unroll_to_tpl()
             pssa01 = MCTSScheduler(drop_list, max_dop=mdp, max_calc_time=0.25)
-            num_parts_done, lpl, ptime, parts = pssa01.partition_dag()
-            #logger.info("MCTS (no deadline): {3} partitioned: parts = {0}, lpl = {1}, ptime = {2:.2f}".format(num_parts_done, lpl, ptime, lgn))
-    #         # pssa02 = SAScheduler(drop_list, max_dop=mdp, deadline=tgt_deadline[j])
-    #         # num_parts_done, lpl, ptime, parts = pssa02.partition_dag()
-    #         #print "SA (deadline): {3} partitioned: parts = {0}, lpl = {1}, deadline = {4}, ptime = {2:.2f}".format(num_parts_done, lpl, ptime, lgn, tgt_deadline[j])
+            pssa01.partition_dag()
