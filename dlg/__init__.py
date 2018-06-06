dc18a40020f8639a49a756b23a166d88bcffda6d
Rodrigo Tobar <rtobar@icrar.org>
2018-06-06 15:14:04 +0800

    Perform logger class setup earlier
    We need to setup our custom logging class before anything else happens.
    We were previously importing names from some internal dlg modules into
    the dlg package __init__ module *before* setting up the logging class,
    and this meant that loggers created in those internal modules at import
    time were not of the new type. This in turn meant that log records did
    not have the additional drop_uid and session_id fields on them, leading
    to errors while formatting log records to output them into the file and
    stream handlers.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- dlg/__init__.py
+++ dlg/__init__.py
@@ -24,45 +24,50 @@ This package contains the modules implementing the core functionality of
 the system.
 """
 
+# It is of utmost importance that this is the *first* thing we do any time
+# we load daliuge up. Most modules in daliuge that have a logger create it at
+# import time, so we need to perform this setup before doing any other imports
+def setup_logger_class():
+    import logging
+
+    # To avoid 'No handlers could be found for logger' messages during testing
+    logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+    # Use our own logger class, which knows about the currently executing app
+    class _DlgLogger(logging.Logger):
+        def makeRecord(self, *args, **kwargs):
+            record = super(_DlgLogger, self).makeRecord(*args, **kwargs)
+
+            from . import drop
+            from .manager import session
+
+            # Try to get the UID of the drop ultimately in charge of sending this
+            # log record, if any
+            try:
+                drop = drop.track_current_drop.tlocal.drop
+            except AttributeError:
+                drop = None
+            drop_uid = drop.uid if drop else ''
+
+            # Do the same with the session_id, which can be found via the drop (if any)
+            # or checking if there is a session currently executing something
+            session_id = ''
+            if drop and hasattr(drop, '_dlg_session') and drop._dlg_session:
+                session_id = drop._dlg_session.sessionId
+            else:
+                try:
+                    session_id = session.track_current_session.tlocal.session.sessionId
+                except AttributeError:
+                    pass
+            record.drop_uid = drop_uid
+            record.session_id = session_id
+            return record
+
+    logging.setLoggerClass(_DlgLogger)
+setup_logger_class()
+del setup_logger_class
+
 from .version import git_version as __git_version__
 from .version import version as __version__
 from .apps import get_include_dir
 from .dask_emulation import delayed
-
-import logging
-
-# To avoid 'No handlers could be found for logger' messages during testing
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-# Use our own logger class, which knows about the currently executing app
-class _DlgLogger(logging.Logger):
-    def makeRecord(self, *args, **kwargs):
-        record = super(_DlgLogger, self).makeRecord(*args, **kwargs)
-
-        from . import drop
-        from .manager import session
-
-        # Try to get the UID of the drop ultimately in charge of sending this
-        # log record, if any
-        try:
-            drop = drop.track_current_drop.tlocal.drop
-        except AttributeError:
-            drop = None
-        drop_uid = drop.uid if drop else ''
-
-        # Do the same with the session_id, which can be found via the drop (if any)
-        # or checking if there is a session currently executing something
-        session_id = ''
-        if drop and hasattr(drop, '_dlg_session') and drop._dlg_session:
-            session_id = drop._dlg_session.sessionId
-        else:
-            try:
-                session_id = session.track_current_session.tlocal.session.sessionId
-            except AttributeError:
-                pass
-        record.drop_uid = drop_uid
-        record.session_id = session_id
-        return record
-
-logging.setLoggerClass(_DlgLogger)
-del logging
