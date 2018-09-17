0b02772dd9ce14b475394141de3da0c0ea73b07c
Rodrigo Tobar <rtobar@icrar.org>
2018-09-17 16:54:20 +0800

    Constraining zeroconf versions for python 3.4
    zeroconf 0.21.0 claims to be compatible with python 3.4 but it isn't
    (see https://github.com/jstasiak/python-zeroconf/issues/139 for
    details), and therefore we need to constrain it.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- setup.py
+++ setup.py
@@ -174,8 +174,13 @@ else:
 
 # Python 2 support has been dropped in zeroconf 0.20.
 # Also, 0.19.0 requires netifaces < 0.10.5, exactly the opposite of what *we* need
+# Also, 0.21.0 erroneously tags 3.4 as supported, while in fact it isn't
+# (see https://github.com/jstasiak/python-zeroconf/issues/139 for details).
+# We provided a fix for that, so from 0.21.1 is already good.
 if sys.version_info[:2] == (2, 7):
     install_requires.append("zeroconf == 0.19.1")
+elif sys.version_info[:2] <= (3, 4):
+    install_requires.append("zeroconf != 0.21.0")
 else:
     install_requires.append("zeroconf >= 0.19.1")
 
