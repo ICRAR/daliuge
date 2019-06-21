af9b7517c642eb1a1d540831bf5a360444622feb
dpallot <dave.pallot@uwa.edu.au>
2019-06-21 15:58:02 +0800

    Refined meta definitions

--- dlg/meta.py
+++ dlg/meta.py
@@ -22,10 +22,34 @@
 
 import collections
 
-dlg_component = collections.namedtuple('dlg_component', 'description inputs outputs')
 dlg_bool_param = collections.namedtuple('dlg_bool_param', 'description default_value')
 dlg_int_param = collections.namedtuple('dlg_int_param', 'description default_value')
 dlg_float_param = collections.namedtuple('dlg_float_param', 'description default_value')
 dlg_string_param = collections.namedtuple('dlg_string_param', 'description default_value')
 dlg_list_param = collections.namedtuple('dlg_list_param', 'description default_value')
 dlg_dict_param = collections.namedtuple('dlg_dict_param', 'description default_value')
+
+
+class dlg_batch_input(object):
+    def __init__(self, mime_type, input_drop_class):
+        self.input_drop_class = input_drop_class
+        self.mime_type = mime_type
+
+
+class dlg_batch_output(object):
+    def __init__(self, mime_type, output_drop_class):
+        self.output_drop_class = output_drop_class
+        self.mime_type = mime_type
+
+
+class dlg_streaming_input(object):
+    def __init__(self, mime_type):
+        self.mime_type = mime_type
+
+
+class dlg_component(object):
+    def __init__(self, description, batch_inputs, batch_outputs, streaming_inputs):
+        self.description = description
+        self.batch_inputs = batch_inputs
+        self.batch_outputs = batch_outputs
+        self.streaming_inputs = streaming_inputs
