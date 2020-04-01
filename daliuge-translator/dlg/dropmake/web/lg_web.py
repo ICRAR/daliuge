ad3ca54313908de9b989a1d4cdf822e540975c3b
james-strauss-uwa <james.strauss@uwa.edu.au>
2020-04-01 13:49:50 +0800

    Adding steps to CWL workflow.

--- daliuge-translator/dlg/dropmake/web/lg_web.py
+++ daliuge-translator/dlg/dropmake/web/lg_web.py
@@ -537,39 +537,32 @@ def create_cwl_workflow(pg_spec, cwl_path, zip_path):
         print(str(node['lg_key']) + " " + str(node['nm']))
 
         if node['dt'] == 'BashShellApp':
+            # create command line tool description
             filename = "step" + str(index) + ".cwl"
             create_command_line_tool(node, pgt_path(filename))
             step_files.append(pgt_path(filename))
+
+            # add step
+            step = cwlgen.WorkflowStep(str(node['nm']), run=filename)
+            cwl_workflow.steps.append(step)
+
 
     # save CWL to path
     with open(cwl_path, "w") as f:
         f.write(cwl_workflow.export_string())
 
-    # add all files together into zip
+    # put workflow and command line tool description files all together in a zip
     zipObj = ZipFile(zip_path, 'w')
-
-    # Add multiple files to the zip
     for step_file in step_files:
         zipObj.write(step_file)
-
     zipObj.write(cwl_path)
-
-    # close the Zip File
     zipObj.close()
 
 
 def create_command_line_tool(node, filename):
     print("create_command_line_tool(" + node['nm'] + "," + filename + ")")
 
-    cwl_tool = cwlgen.CommandLineTool(tool_id='grep', label='print lines matching a pattern', base_command='grep', cwl_version='v1.0')
-
-    file_binding = cwlgen.CommandLineBinding(position=2)
-    input_file = cwlgen.CommandInputParameter('input_file', param_type='File', input_binding=file_binding, doc='input file from which you want to look for the pattern')
-    cwl_tool.inputs.append(input_file)
-
-    pattern_binding = cwlgen.CommandLineBinding(position=1)
-    pattern = cwlgen.CommandInputParameter('pattern', param_type='string', input_binding=pattern_binding, doc='pattern to find in the input file')
-    cwl_tool.inputs.append(pattern)
+    cwl_tool = cwlgen.CommandLineTool(tool_id=node['app'], label=node['nm'], base_command=node['command'], cwl_version='v1.0')
 
     # write to file
     with open(filename, "w") as f:
