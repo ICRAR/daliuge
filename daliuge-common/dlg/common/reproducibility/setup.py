b1682211bd3231f6a559c3fa3084797662e123a8
pritchardn <21726929@student.uwa.edu.au>
2020-06-02 10:41:12 +0800

    Adds stub functions for reproducibility functions. Broadly speaking, this is where most of the magic will happen.

--- daliuge-common/dlg/common/reproducibility/setup.py
+++ daliuge-common/dlg/common/reproducibility/setup.py
@@ -1,25 +1,56 @@
 from dlg.common.reproducibility.constants import ReproduciblityFlags, REPRO_DEFAULT, rmode_supported
 
 
-def initialize_lg_data(lg: dict, level: str):
+#  ------ Drop-Based Functionality ------
+
+def init_lgt_repro_drop_data(drop: dict, level: ReproduciblityFlags):
+    pass
+
+
+def init_lg_repro_drop_data(drop: dict, level: ReproduciblityFlags):
+    pass
+
+
+def init_pgt_repro_drop_data(drop: dict, level: ReproduciblityFlags):
+    pass
+
+
+def init_pg_repro_drop_data(drop: dict, level: ReproduciblityFlags):
+    pass
+
+
+#  ------ Graph-Wide Functionality ------
+
+def init_lgt_repro_data(lgt: dict, rmode: str):
     """
     Creates and appends graph-wide reproducibility data at the logical template stage.
     Currently, this is basically a stub that adds the requested flag to the graph.
     Later, this will contain significantly more information.
-    :param lg: The logical graph data structure (a JSON object (a dict))
-    :param level: One several values 0-5 defined in constants.py
+    :param lgt: The logical graph data structure (a JSON object (a dict))
+    :param rmode: One several values 0-5 defined in constants.py
     :return: The same lg object with new information appended
 
     TODO: Per-drop initialization
     TODO: Cryptographic processing of structure
-    TODO: Definition of behaviour
     """
-    level = ReproduciblityFlags(level)
-    if not rmode_supported(level):
-        level = REPRO_DEFAULT
+    rmode = ReproduciblityFlags(rmode)
+    if not rmode_supported(rmode):
+        rmode = REPRO_DEFAULT
     reprodata = {
-        'level': level
+        'rmode': rmode
     }
-    lg['reproData'] = reprodata
+    lgt['reproData'] = reprodata
 
-    return lg
+    return lgt
+
+
+def init_lg_repro_data(lg: dict, level: str):
+    pass
+
+
+def init_pgt_repro_data(pgt: dict, level: str):
+    pass
+
+
+def init_pg_repro_data(pg: dict, level: str):
+    pass
