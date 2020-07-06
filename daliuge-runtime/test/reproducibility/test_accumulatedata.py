8b8ae3916a592857eef244e89a7fe4064c898e8f
pritchardn <21726929@student.uwa.edu.au>
2020-07-06 14:34:35 +0800

    Beginning to implement testing procedures

---
+++ daliuge-runtime/test/reproducibility/test_accumulatedata.py
@@ -0,0 +1,98 @@
+import json
+import unittest
+
+from dlg.common.reproducibility.constants import ReproduciblityFlags
+from dlg.common.reproducibility.reproducibility import accumulate_lgt_drop_data
+
+
+class AccumulateLGTRerunData(unittest.TestCase):
+    # Locations in apps.graph for various application types
+    bash_app = 0
+    dyn_lib = 1
+    mpi = 2
+    docker = 3
+    python = 4
+
+    def test_app_accumulate(self):
+        fp = open('reproGraphs/apps.graph')
+        node_data = json.load(fp)['nodeDataArray']
+        app_hashes = []
+        for i in range(5):
+            app = node_data[i]
+            hash_data = accumulate_lgt_drop_data(app, ReproduciblityFlags.RERUN)
+            print(hash_data)
+            #app_hashes.append(HASHING_ALG(hash_data).hexdigest()))
+        fp.close()
+        assert True
+
+    def test_data_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_group_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_control_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_other_accumulate(self):
+        self.assertEqual(True, False)
+
+
+class AccumulateLGRerunData(unittest.TestCase):
+    def test_all_accumulate(self):
+        self.assertEqual(True, False)
+
+
+class AccumulatePGTUnrollRerunData(unittest.TestCase):
+    def test_app_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_data_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_group_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_control_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_other_accumulate(self):
+        self.assertEqual(True, False)
+
+
+class AccumulatePGTPartitionRerunData(unittest.TestCase):
+    def test_app_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_data_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_group_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_control_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_other_accumulate(self):
+        self.assertEqual(True, False)
+
+
+class AccumulatePGRerunData(unittest.TestCase):
+    def test_app_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_data_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_group_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_control_accumulate(self):
+        self.assertEqual(True, False)
+
+    def test_other_accumulate(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
