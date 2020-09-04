470ba0e6591338571891e5817a1a965a4e4ea349
pritchardn <nicholas.pritchard@research.uwa.edu.au>
2020-09-04 10:39:58 +0800

    Fixes typo in documentation

--- daliuge-common/dlg/common/reproducibility/constants.py
+++ daliuge-common/dlg/common/reproducibility/constants.py
@@ -22,7 +22,7 @@ def rmode_supported(flag: ReproducibilityFlags):
     """
     Determines in a given flag is currently supported.
     A slightly pedantic solution but it does centralize the process.
-    There is the possiblity that different functionality is possible on a per-install basis.
+    There is the possibility that different functionality is possible on a per-install basis.
     Named to be used as a if rmode_supported(flag)
     :param flag: A ReproducibilityFlag enum being queried
     :return: True if supported, False otherwise
