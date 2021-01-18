7509ccb2c73ecbc0951fb29fdf89a7f2b494652b
pritchardn <21726929@student.uwa.edu.au>
2021-01-18 12:58:13 +0800

    Adds add_noise drop to Lowpass example.

--- daliuge-common/dlg/common/reproducibility/apps_lowpass.py
+++ daliuge-common/dlg/common/reproducibility/apps_lowpass.py
@@ -1,10 +1,11 @@
 import pickle
 
 import numpy as np
+from dlg import droputils
 from dlg.apps.simple import BarrierAppDROP
 from dlg.meta import dlg_batch_output, dlg_streaming_input
 from dlg.meta import dlg_component, dlg_batch_input
-from dlg.meta import dlg_int_param, dlg_list_param
+from dlg.meta import dlg_int_param, dlg_list_param, dlg_float_param
 from merklelib import MerkleTree
 
 
@@ -101,3 +102,59 @@ class LP_WindowGenerator(BarrierAppDROP):
 
     def generate_reproduce_data(self):
         return {'data_hash', MerkleTree(self.series).merkle_root}
+
+
+class LP_AddNoise(BarrierAppDROP):
+    component_meta = dlg_component('LPAddNoise', 'Adds noise to a signal generated for the low-pass filter example',
+                                   [dlg_batch_input('binary/*', [])],
+                                   [dlg_batch_output('binary/*', [])],
+                                   [dlg_streaming_input('binary/*')])
+
+    # default values
+    mean = dlg_float_param('avg_noise', 0.0)
+    std = dlg_float_param('std_deviation', 1.0)
+    freq = dlg_int_param('frequency', 1200)
+    srate = dlg_int_param('sample_rate', 5000)
+    seed = dlg_int_param('random_seed', 42)
+    alpha = dlg_float_param('noise_multiplier', 0.1)
+    signal = np.empty([1])
+
+    def initialize(self, **kwargs):
+        super(LP_AddNoise).initialize(**kwargs)
+
+    def add_noise(self):
+        np.random.seed(self.seed)
+        samples = self.alpha * np.random.normal(self.mean, self.std, size=len(self.signal))
+        for i in range(len(self.signal)):
+            samples[i] += np.sin(2 * np.pi * i * self.freq / self.srate)
+        np.add(self.signal, samples, out=self.signal)
+        return self.signal
+
+    def getInputArrays(self):
+        ins = self.inputs
+        if len(ins) != 1:
+            raise Exception('Precisely one input required for %r' % self)
+
+        array = pickle.loads(droputils.allDropContents(ins[0]))
+        self.signal = array
+
+    def run(self):
+        outs = self.outputs
+        if len(outs) < 1:
+            raise Exception('At least one output required for %r' % self)
+        self.getInputArrays()
+        sig = self.add_noise()
+        data = pickle.dumps(sig)
+        for o in outs:
+            o.len = len(data)
+            o.write(data)
+
+    def generate_recompute_data(self):
+        return {'mean': self.mean,
+                'std': self.std,
+                'sample_rate': self.srate,
+                'seed': self.seed,
+                'alpha': self.alpha}
+
+    def generate_reproduce_data(self):
+        return {'data_hash', MerkleTree(self.signal).merkle_root}
