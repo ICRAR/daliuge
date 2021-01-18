e40ab0262061baa1b96cc1d5abd99f9f2d444a43
pritchardn <21726929@student.uwa.edu.au>
2021-01-18 12:21:53 +0800

    Separates Lowpass example specific apps into apps_lowpass.py

---
+++ daliuge-common/dlg/common/reproducibility/apps_lowpass.py
@@ -0,0 +1,103 @@
+import pickle
+
+import numpy as np
+from dlg.apps.simple import BarrierAppDROP
+from dlg.meta import dlg_batch_output, dlg_streaming_input
+from dlg.meta import dlg_component, dlg_batch_input
+from dlg.meta import dlg_int_param, dlg_list_param
+from merklelib import MerkleTree
+
+
+class LP_SignalGenerator(BarrierAppDROP):
+    component_meta = dlg_component('LPSignalGen', 'Low-pass filter example signal generator',
+                                   [dlg_batch_input('binary/*', [])],
+                                   [dlg_batch_output('binary/*', [])],
+                                   [dlg_streaming_input('binary/*')])
+
+    # default values
+    length = dlg_int_param('length', 512)
+    srate = dlg_int_param('sample rate', 5000)
+    freqs = dlg_list_param('Frequencies(int)', [440, 800, 1000, 2000])
+    series = np.empty([1])
+
+    def initialize(self, **kwargs):
+        super(LP_SignalGenerator, self).initialize(**kwargs)
+
+    def gen_sig(self):
+        series = np.zeros(self.length, dtype=np.float64)
+        for freq in self.freqs:
+            for i in range(self.length):
+                series[i] += np.sin(2 * np.pi * i * freq / self.srate)
+        return series
+
+    def run(self):
+        outs = self.outputs
+        if len(outs) < 1:
+            raise Exception('At least one output required for %r' % self)
+        self.series = self.gen_sig()
+        data = pickle.dumps(self.series)
+        for o in outs:
+            o.len = len(data)
+            o.write(data)
+
+    def generate_recompute_data(self):
+        # This will do for now
+        return {'length': self.length,
+                'sample_rate': self.srate,
+                'frequencies': self.freqs}
+
+    def generate_reproduce_data(self):
+        # This will do for now
+        return {'data_hash': MerkleTree(self.series).merkle_root}
+
+
+class LP_WindowGenerator(BarrierAppDROP):
+    component_meta = dlg_component('LPWindowGen', 'Low-pass filter example window generator',
+                                   [dlg_batch_input('binary/*', [])],
+                                   [dlg_batch_output('binary/*', [])],
+                                   [dlg_streaming_input('binary/*')])
+
+    # default values
+    length = dlg_int_param('length', 512)
+    cutoff = dlg_int_param('cutoff', 600)
+    srate = dlg_int_param('sample_rate', 5000)
+    series = np.empty([1])
+
+    def initialize(self, **kwargs):
+        super(LP_WindowGenerator, self).initialize(**kwargs)
+
+    def sinc(self, x_val: np.float64):
+        """
+        Computes the sin_c value for the input float
+        :param x_val:
+        """
+        if np.isclose(x_val, 0.0):
+            return 1.0
+        return np.sin(np.pi * x_val) / (np.pi * x_val)
+
+    def gen_win(self):
+        alpha = 2 * self.cutoff / self.srate
+        win = np.zeros(self.length)
+        for i in range(self.length):
+            ham = 0.54 - 0.46 * np.cos(2 * np.pi * i / self.length)  # Hamming coefficient
+            hsupp = (i - self.length / 2)
+            win[i] = ham * alpha * self.sinc(alpha * hsupp)
+        return win
+
+    def run(self):
+        outs = self.outputs
+        if len(outs) < 1:
+            raise Exception('At least one output required for %r' % self)
+        self.series = self.gen_win()
+        data = pickle.dumps(self.series)
+        for o in outs:
+            o.len = len(data)
+            o.write(data)
+
+    def generate_recompute_data(self):
+        return {'length': self.length,
+                'cutoff': self.cutoff,
+                'sample_rate': self.srate}
+
+    def generate_reproduce_data(self):
+        return {'data_hash', MerkleTree(self.series).merkle_root}
