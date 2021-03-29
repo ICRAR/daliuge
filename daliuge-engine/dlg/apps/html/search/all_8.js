815aab7ded52236f5ce076492470b4c740d591d4
awicenec <awicenec@gmail.com>
2021-03-29 11:37:37 +0800

    Removed HTML, Latex and XML files

--- daliuge-engine/dlg/apps/html/search/all_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['helloworldapp',['HelloWorldApp',['../classdlg_1_1apps_1_1simple_1_1_hello_world_app.html',1,'dlg::apps::simple']]]
-];
--- daliuge-engine/dlg/apps/html/search/classes_b.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pyfuncapp',['PyFuncApp',['../classdlg_1_1apps_1_1pyfunc_1_1_py_func_app.html',1,'dlg::apps::pyfunc']]]
-];
