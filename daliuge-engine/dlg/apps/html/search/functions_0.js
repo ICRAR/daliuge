233fe779e8ff672928046f8137717d27db0beb25
awicenec <awicenec@gmail.com>
2021-03-29 11:37:37 +0800

    Removed HTML, Latex and XML files

--- daliuge-engine/dlg/apps/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['getinputarrays',['getInputArrays',['../classdlg_1_1apps_1_1simple_1_1_average_arrays_app.html#ae0c6c4db425f36093fc053ed75a9db16',1,'dlg::apps::simple::AverageArraysApp']]]
-];
