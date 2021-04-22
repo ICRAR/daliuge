1aca54a9420bfb35dd9dc967623a1a4dedc88df2
pritchardn <21726929@student.uwa.edu.au>
2021-04-22 08:08:36 +0800

    Adds system summary to lowpass apps

--- daliuge-common/dlg/common/reproducibility/constants.py
+++ daliuge-common/dlg/common/reproducibility/constants.py
@@ -1,5 +1,11 @@
 import hashlib
+import platform
+import sys
 from enum import Enum
+
+import GPUtil
+import psutil
+from merklelib import MerkleTree
 
 PROTOCOL_VERSION = 0.1
 
@@ -71,3 +77,55 @@ def rmode_supported(flag: ReproducibilityFlags):
         return True
     else:
         return False
+
+
+def find_loaded_modules():
+    """
+    :return: A list of all loaded modules
+    """
+    loaded_mods = []
+    for name, module in sorted(sys.modules.items()):
+        if hasattr(module, '__version__'):
+            loaded_mods.append(name + " " + str(module.__version__))
+        else:
+            loaded_mods.append(name)
+    return loaded_mods
+
+
+def system_summary():
+    """
+    Summarises the system this function is run on.
+    Includes system, cpu, gpu and module details
+    :return: A dictionary of system details
+    """
+    merkletree = MerkleTree()
+    system_info = {}
+    uname = platform.uname()
+    system_info['system'] = {
+        'system': uname.system,
+        'release': uname.release,
+        'machine': uname.machine,
+        'processor': uname.processor
+    }
+    cpu_freq = psutil.cpu_freq()
+    system_info['cpu'] = {
+        'cores_phys': psutil.cpu_count(logical=False),
+        'cores_logic': psutil.cpu_count(logical=True),
+        'max_frequency': cpu_freq.max,
+        'min_frequency': cpu_freq.min
+    }
+    sys_mem = psutil.virtual_memory()
+    system_info['memory'] = {
+        'total': sys_mem.total
+    }
+    gpus = GPUtil.getGPUs()
+    system_info['gpu'] = {}
+    for gpu in gpus:
+        system_info['gpu'][gpu.id] = {
+            'name': gpu.name,
+            'memory': gpu.memoryTotal
+        }
+    system_info['modules'] = find_loaded_modules()
+    merkletree.append([system_info[item] for item in system_info])
+    system_info['signature'] = merkletree.merkle_root
+    return system_info
