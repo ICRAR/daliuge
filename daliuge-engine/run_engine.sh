407eb9bb1ad7bb5a0aabfd4ced9f567c05ec8022
awicenec <awicenec@gmail.com>
2021-05-12 09:15:36 +0800

    comments and startup changes to support plasma

--- daliuge-engine/run_engine.sh
+++ daliuge-engine/run_engine.sh
@@ -1,1 +1,5 @@
-docker run --rm -td --name daliuge-engine -v /var/run/docker.sock:/var/run/docker.sock -p 5555:5555 -p 6666:6666 -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 9000:9000  icrar/daliuge-engine:ray
+docker run --shm-size=1g --ipc="shareable" --rm -td --name daliuge-engine -v /var/dlg_home:/var/dlg_home -v /var/run/docker.sock:/var/run/docker.sock -p 5555:5555 -p 6666:6666 -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 9000:9000  icrar/daliuge-engine:ray
+
+# start the plasma store. NOTE: the container has been started with shareable memory
+# which means that the plasma store is accessible across containers on the same host.
+plasma_store -m 10000000 -s /var/dlg_home/tmp/plasma -d /var/dlg_home/tmp
