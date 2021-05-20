3e5d65587d93cda4b6e57202f7ca10d4f5535559
awicenec <awicenec@gmail.com>
2021-05-20 10:11:47 +0800

    updates to the run script

--- daliuge-engine/run_engine.sh
+++ daliuge-engine/run_engine.sh
@@ -1,5 +1,5 @@
 DOCKER_OPTS="\
---shm-size=1g --ipc=\"shareable\" \
+--shm-size=1g --ipc=shareable \
 --rm \
 --name daliuge-engine \
 -v /var/dlg_home:/var/dlg_home \
@@ -20,20 +20,13 @@ case "$1" in
         export VCS_TAG=`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`
         echo "Running Engine development version in foreground..."
         DOCKER_OPTS+="-v ${PWD}/dlg/manager:/home/ray/dlg/lib/python3.8/site-packages/dlg/manager"
-        echo "docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}"
+        echo "docker run -ti ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}"
         docker run -ti ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}
         exit 1;;
     *)
         echo "Usage run_engine.sh <dep|dev>"
         exit 1;;
 esac
-
-
-docker run --shm-size=1g --ipc="shareable" --rm -td --name daliuge-engine -v /var/dlg_home:/var/dlg_home -v /var/run/docker.sock:/var/run/docker.sock -p 5555:5555 -p 6666:6666 -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 9000:9000  icrar/daliuge-engine:ray
-sleep 2
-# ./start_local_managers.sh
-# start the plasma store. NOTE: the container has been started with shareable memory
-# which means that the plasma store is accessible across containers on the same host.
 
 # TODO: The following command just leaves a defunct process, but not the plasma store running (problem of quoting??)
 # docker  exec -t daliuge-engine bash -c 'plasma_store -m 600000000 -s /var/dlg_home/tmp/plasma 1> /var/dlg_home/logs/plasma 2> /var/dlg_home/logs/plasma &'
