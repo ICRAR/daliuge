1d9b5d12d3e117512d7f949e2bb5761aaf9cfddc
awicenec <awicenec@gmail.com>
2021-05-20 10:09:38 +0800

    added development and deployment options to docker build and run

--- daliuge-engine/run_engine.sh
+++ daliuge-engine/run_engine.sh
@@ -1,3 +1,34 @@
+DOCKER_OPTS="\
+--shm-size=1g --ipc=\"shareable\" \
+--rm \
+--name daliuge-engine \
+-v /var/dlg_home:/var/dlg_home \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-p 5555:5555 -p 6666:6666 \
+-p 8000:8000 -p 8001:8001 \
+-p 8002:8002 -p 9000:9000 \
+" 
+
+case "$1" in
+    "dep")
+        VCS_TAG=`git describe --tags --abbrev=0|sed s/v//`
+        echo "Running Engine deployment version in background..."
+        echo "docker run -td "${DOCKER_OPTS}"  icrar/daliuge-engine:${VCS_TAG}"
+        docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}
+        exit 1;;
+    "dev")
+        export VCS_TAG=`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`
+        echo "Running Engine development version in foreground..."
+        DOCKER_OPTS+="-v ${PWD}/dlg/manager:/home/ray/dlg/lib/python3.8/site-packages/dlg/manager"
+        echo "docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}"
+        docker run -ti ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}
+        exit 1;;
+    *)
+        echo "Usage run_engine.sh <dep|dev>"
+        exit 1;;
+esac
+
+
 docker run --shm-size=1g --ipc="shareable" --rm -td --name daliuge-engine -v /var/dlg_home:/var/dlg_home -v /var/run/docker.sock:/var/run/docker.sock -p 5555:5555 -p 6666:6666 -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 9000:9000  icrar/daliuge-engine:ray
 sleep 2
 # ./start_local_managers.sh
