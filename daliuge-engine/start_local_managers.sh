ccc3289c368b8c33580c3ce39f2d2fa9f5a56ea9
awicenec <awicenec@gmail.com>
2021-05-20 10:09:38 +0800

    added development and deployment options to docker build and run
    Former-commit-id: 949021792992a90420ceee58102ca3b688f34272
    Former-commit-id: 341e78d50bb4368d454addeda66c32e8e21b86b5

--- daliuge-engine/start_local_managers.sh
+++ daliuge-engine/start_local_managers.sh
@@ -1,5 +1,5 @@
 # Script starts a node manager and a data island manager on the local node. Useful mainly for testing.
 
 # for some reason the -d flag does no work with the dim always leaves a defunct process
-# docker exec daliuge-engine bash -c 'dlg nm -vvd --no-dlm -H 0.0.0.0 -w /var/dlg_home/workspace'
+# docker exec daliuge-engine bash -c 'dlg nm -vvd --no-dlm -H 0.0.0.0 --dlg-path=/var/dlg_home/code -w /var/dlg_home/workspace'
 docker exec -ti daliuge-engine bash -c 'dlg dim -N localhost -vv -H 0.0.0.0 -w /var/dlg_home/workspace > /tmp/dim.log 2&>1 &'
