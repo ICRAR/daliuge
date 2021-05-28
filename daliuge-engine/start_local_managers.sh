e715bde1f5101612f0b82f36ef61d5b7d9012ea2
awicenec <awicenec@gmail.com>
2021-05-28 16:23:00 +0800

    Forgot to push one change in drop.py
    Former-commit-id: 51eb308a2696456147ea431446ad32737107ab9a
    Former-commit-id: 7ee2d1988e1589f0b3f653e28ad626fe5dd2a961

--- daliuge-engine/start_local_managers.sh
+++ daliuge-engine/start_local_managers.sh
@@ -1,6 +1,2 @@
 # Script starts a node manager and a data island manager on the local node. Useful mainly for testing.
-
-# for some reason the -d flag does no work with the dim always leaves a defunct process
-# docker exec daliuge-engine bash -c 'dlg nm -vvd --no-dlm -H 0.0.0.0 --dlg-path=/var/dlg_home/code -w /var/dlg_home/workspace'
-docker exec -ti daliuge-engine bash -c 'dlg nm -vv --no-dlm -H 0.0.0.0 --dlg-path=/var/dlg_home/code -w /var/dlg_home/workspace > /tmp/nm.log 2>&1 &'
-docker exec -ti daliuge-engine bash -c 'dlg dim -N localhost -vv -H 0.0.0.0 -w /var/dlg_home/workspace > /tmp/dim.log 2>&1 &'
+curl -d '{"nodes": ["localhost"]}' -H "Content-Type: application/json" -X POST http://localhost:9000/managers/dataisland
