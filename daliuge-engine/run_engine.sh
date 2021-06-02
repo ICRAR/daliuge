186d360d9f38dd6b2bdb58eaf660ee445775ea64
awicenec <awicenec@gmail.com>
2021-06-02 16:03:19 +0800

    Few modifications to fix Markus' issues getting this running

--- daliuge-engine/run_engine.sh
+++ daliuge-engine/run_engine.sh
@@ -2,7 +2,6 @@ DOCKER_OPTS="\
 --shm-size=1g --ipc=shareable \
 --rm \
 --name daliuge-engine \
--v /var/dlg_home:/var/dlg_home \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -p 5555:5555 -p 6666:6666 \
 -p 8000:8000 -p 8001:8001 \
@@ -12,6 +11,7 @@ DOCKER_OPTS="\
 case "$1" in
     "dep")
         VCS_TAG=`git describe --tags --abbrev=0|sed s/v//`
+        DOCKER_OPTS+="-v /var/dlg_home:/var/dlg_home"
         echo "Running Engine deployment version in background..."
         echo "docker run -td "${DOCKER_OPTS}"  icrar/daliuge-engine:${VCS_TAG}"
         docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}
@@ -19,14 +19,17 @@ case "$1" in
     "dev")
         export VCS_TAG=`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`
         echo "Running Engine development version in background..."
+        mkdir -p /tmp/.dlg/var/dlg_home/workspace
+        mkdir -p /tmp/.dlg/var/dlg_home/testdata
+        mkdir -p /tmp/.dlg/var/dlg_home/code
         DOCKER_OPTS+="-v ${PWD}/dlg/manager:/root/dlg/lib/python3.8/site-packages/dlg/manager"
+        DOCKER_OPTS+=" -v /tmp/.dlg/var/dlg_home:/tmp/.dlg/var/dlg_home"
         echo "docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}"
         docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}
+        sleep 3
+        ./start_local_managers.sh
         exit 1;;
     *)
         echo "Usage run_engine.sh <dep|dev>"
         exit 1;;
 esac
-
-# TODO: The following command just leaves a defunct process, but not the plasma store running (problem of quoting??)
-# docker  exec -t daliuge-engine bash -c 'plasma_store -m 600000000 -s /var/dlg_home/tmp/plasma 1> /var/dlg_home/logs/plasma 2> /var/dlg_home/logs/plasma &'
