e634286304e8c8b87bea239e2096cf2c25de108b
awicenec <awicenec@gmail.com>
2021-06-10 21:59:18 +0800

    Aligned docker build procedures
    Former-commit-id: 4e2dc8b4af37fd42a8a44f47efce01ffb1c065ac
    Former-commit-id: 627b464a5ebe4c6b8ad1b8e720d9b91fe7945995

--- daliuge-engine/build_engine.sh
+++ daliuge-engine/build_engine.sh
@@ -7,18 +7,18 @@ case "$1" in
     "dep")
         export VCS_TAG=`git describe --tags --abbrev=0|sed s/v//`
         echo "Building daliuge-engine version ${VCS_TAG}"
-        docker build --no-cache -t icrar/daliuge-engine:${VCS_TAG} -f docker/Dockerfile .
+        docker build --build-arg VCS_TAG=${VCS_TAG} --no-cache -t icrar/daliuge-engine:${VCS_TAG} -f docker/Dockerfile .
         echo "Build finished!"
         exit 1 ;;
     "dev")
         export VCS_TAG=`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`
-        echo "Building daliuge-engine development version"
+        echo "Building daliuge-engine development version using tag ${VCS_TAG}"
         # The complete casa and arrow installation is only required for the Plasma streaming
         # and should not go much further.
-        docker build --no-cache -t icrar/daliuge-engine:${VCS_TAG} -f docker/Dockerfile.casa .
+        docker build --build-arg VCS_TAG=${VCS_TAG} --no-cache -t icrar/daliuge-engine:${VCS_TAG} -f docker/Dockerfile .
         echo "Build finished!"
         exit 1;;
     *)
-        echo "Usage: build_eagle.sh <dep|dev>"
+        echo "Usage: build_engine.sh <dep|dev>"
         exit 1;;
 esac
