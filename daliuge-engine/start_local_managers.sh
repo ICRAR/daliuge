dc06dc70e1b25be9cd47532bcea2b854520b4101
awicenec <awicenec@gmail.com>
2021-06-10 16:50:05 +0800

    Final commit for this ticket
    Former-commit-id: 9290db396822fe7a9c4faec7240f4af3e136325e
    Former-commit-id: 9b8776093a94d9502dd7393b9aa991b56622becb

--- daliuge-engine/start_local_managers.sh
+++ daliuge-engine/start_local_managers.sh
@@ -1,2 +1,3 @@
 # Script starts a node manager and a data island manager on the local node. Useful mainly for testing.
+# docker exec -ti daliuge-engine /daliuge/
 curl -d '{"nodes": ["localhost"]}' -H "Content-Type: application/json" -X POST http://localhost:9000/managers/dataisland
