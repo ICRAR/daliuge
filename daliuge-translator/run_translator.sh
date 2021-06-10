b12035ff3a3fd639303dee6403874b0eadc607b0
awicenec <awicenec@gmail.com>
2021-06-10 10:26:50 +0800

    Start local managers in dev mode
    Former-commit-id: 7f427a3ce7d452c8dca26ace4f1c58353ad0507c
    Former-commit-id: 444d52acd5777de38343dbc845e32d40c2844da6

--- daliuge-translator/run_translator.sh
+++ daliuge-translator/run_translator.sh
@@ -7,7 +7,7 @@ case "$1" in
     "dev")
         export VCS_TAG=`git rev-parse --abbrev-ref HEAD`
         echo "Running Translator development version in foreground..."
-        docker run --volume $PWD/dlg/dropmake:/home/ray/dlg/lib/python3.8/site-packages/dlg/dropmake --name daliuge-translator --rm -t -p 8084:8084 icrar/daliuge-translator:ray
+        docker run --volume $PWD/dlg/dropmake:/home/ray/dlg/lib/python3.8/site-packages/dlg/dropmake --name daliuge-translator --rm -t -p 8084:8084 icrar/daliuge-translator:yan-708
         exit 1;;
     *)
         echo "Usage run_translator.sh <dep|dev>"
