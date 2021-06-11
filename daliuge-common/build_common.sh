a66635b57d0c4f5d237add4b49f3ec433a8ef131
M-Wicenec <wicenecmw@gmail.com>
2021-06-11 10:10:54 +0800

    Test

--- daliuge-common/build_common.sh
+++ daliuge-common/build_common.sh
@@ -16,7 +16,7 @@ case "$1" in
         echo "Building daliuge-common development version using tag ${VCS_TAG}"
         # The complete casa and arrow installation is only required for the Plasma streaming
         # and should not go much further.
-        docker build --build-arg VCS_TAG=${VCS_TAG} --no-cache -t icrar/daliuge-common:${VCS_TAG} -f docker/Dockerfile.${VCS_TAG} .
+        docker build --build-arg VCS_TAG=${VCS_TAG} --no-cache -t icrar/daliuge-common:${VCS_TAG} -f docker/Dockerfile.dev .
         echo "Build finished!"
         exit 1;;
     *)
@@ -24,28 +24,3 @@ case "$1" in
         exit 1;;
 esac
 
-<<<<<<< HEAD
-#!/bin/bash
-# script builds the daliuge-common docker container either with a tag referring to the current
-# branch name or with a release tag depending whether this is a development or deployment
-# version.
-
-case "$1" in
-    "dep")
-        export VCS_TAG=`git describe --tags --abbrev=0|sed s/v//`
-        echo "Building daliuge-common version ${VCS_TAG}"
-        docker build --no-cache -t icrar/daliuge-common:${VCS_TAG} -f docker/Dockerfile .
-        echo "Build finished!"
-        exit 1 ;;
-    "dev")
-        export VCS_TAG=`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`
-        echo "Building daliuge-common development version"
-        docker build --no-cache -t icrar/daliuge-common:${VCS_TAG} -f docker/Dockerfile.dev .
-        echo "Build finished!"
-        exit 1;;
-    *)
-        echo "Usage: build_common.sh <dep|dev>"
-        exit 1;;
-esac
-=======
->>>>>>> YAN-708
