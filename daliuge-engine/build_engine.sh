f3064e2c4a20e1eef382124a2149464ee9b05f10
M-Wicenec <wicenecmw@gmail.com>
2021-06-11 10:10:54 +0800

    Test
    Former-commit-id: 7a862902c192a736ec4654f0ffda67419c6ae99b
    Former-commit-id: fcfd2f994e9b79cff4c6614deb1645964b733637

--- daliuge-engine/build_engine.sh
+++ daliuge-engine/build_engine.sh
@@ -6,21 +6,14 @@
 case "$1" in
     "dep")
         export VCS_TAG=`git describe --tags --abbrev=0|sed s/v//`
-        echo "Building daliuge-engine version ${VCS_TAG}"
+        echo "Building daliuge-engine version using tag ${VCS_TAG}"
         docker build --build-arg VCS_TAG=${VCS_TAG} --no-cache -t icrar/daliuge-engine:${VCS_TAG} -f docker/Dockerfile .
         echo "Build finished!"
         exit 1 ;;
     "dev")
         export VCS_TAG=`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`
-<<<<<<< HEAD
         echo "Building daliuge-engine development version using tag ${VCS_TAG}"
-        # The complete casa and arrow installation is only required for the Plasma streaming
-        # and should not go much further.
         docker build --build-arg VCS_TAG=${VCS_TAG} --no-cache -t icrar/daliuge-engine:${VCS_TAG} -f docker/Dockerfile .
-=======
-        echo "Building daliuge-engine development version"
-        docker build --no-cache -t icrar/daliuge-engine:${VCS_TAG} -f docker/Dockerfile .
->>>>>>> c3a5787b2f7cb82795fad8617c8e3bcc5c5c7495
         echo "Build finished!"
         exit 1;;
     *)
