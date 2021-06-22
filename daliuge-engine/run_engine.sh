c688baf19af74a428b7b2c31fce86c3955e5be49
awicenec <awicenec@gmail.com>
2021-06-22 16:26:34 +0800

    Final merge with YAN-708 also reflecting new temporary directory location

--- daliuge-engine/run_engine.sh
+++ daliuge-engine/run_engine.sh
@@ -30,6 +30,19 @@ case "$1" in
         sleep 3
         ./start_local_managers.sh
         exit 1;;
+    "casa")
+        export VCS_TAG=`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`
+        echo "Running Engine development version in background..."
+        mkdir -p /tmp/.dlg/var/dlg_home/workspace
+        mkdir -p /tmp/.dlg/var/dlg_home/testdata
+        mkdir -p /tmp/.dlg/var/dlg_home/code
+        DOCKER_OPTS+="-v ${PWD}/dlg/manager:/root/dlg/lib/python3.8/site-packages/dlg/manager"
+        DOCKER_OPTS+=" -v /tmp/.dlg/var/dlg_home:/tmp/.dlg/var/dlg_home"
+        echo "docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}-casa"
+        docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}-casa
+        sleep 3
+        ./start_local_managers.sh
+        exit 1;;
     *)
         echo "Usage run_engine.sh <dep|dev>"
         exit 1;;
