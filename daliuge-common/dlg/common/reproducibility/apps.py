1778ae87efb49103249d47076846ccd99fe6c95f
pritchardn <21726929@student.uwa.edu.au>
2021-07-19 14:53:02 +0800

    Style adjustments by pylint for apps.py

--- daliuge-common/dlg/common/reproducibility/apps.py
+++ daliuge-common/dlg/common/reproducibility/apps.py
@@ -1,45 +1,83 @@
+"""
+Contains several very basic apps to test python function reproducibility.
+"""
+import numpy as np
+
 from dlg.apps.pyfunc import PyFuncApp
 
 
-def writeIn():
+def write_in():
+    """
+    :return: "world" always
+    """
     return "world"
 
 
-def writeOut(s="everybody"):
-    return "Hello " + s
+def write_out(phrase="everybody"):
+    """
+    Appends s to "Hello "
+    :param phrase: The string to be appended
+    :return: "Hello " + s
+    """
+    return "Hello " + phrase
 
 
 def numpy_av(nums):
-    import numpy as np
+    """
+    Finds the mean of a list of numbers using numpy.
+    :param nums: The numbers to be averaged.
+    :return: The mean.
+    """
     return np.asscalar(np.mean(nums))
 
 
 def my_av(nums):
+    """
+    Finds the mean of a list of numbers manually
+    :param nums: The numbers to be averaged
+    :return: The mean.
+    """
     res = 0.0
-    for x in nums:
-        res += x
+    for num in nums:
+        res += num
     return res / len(nums)
 
 
 class HelloWorldPythonIn(PyFuncApp):
+    """
+    Wrapper app turning writeIn into a Python function app
+    """
+
     def initialize(self, **kwargs):
-        fname = 'dlg.common.reproducibility.apps.writeIn'
-        super(HelloWorldPythonIn, self).initialize(func_name=fname)
+        fname = 'dlg.common.reproducibility.apps.write_in'
+        super().initialize(func_name=fname)
 
 
 class HelloWorldPythonOut(PyFuncApp):
+    """
+    Wrapper app turning writeOut into a Python function app
+    """
+
     def initialize(self, **kwargs):
-        fname = 'dlg.common.reproducibility.apps.writeOut'
-        super(HelloWorldPythonOut, self).initialize(func_name=fname)
+        fname = 'dlg.common.reproducibility.apps.write_out'
+        super().initialize(func_name=fname)
 
 
 class NumpyAverage(PyFuncApp):
+    """
+    Wrapper app turning numpy_av into a Python function app
+    """
+
     def initialize(self, **kwargs):
         fname = 'dlg.common.reproducibility.apps.numpy_av'
-        super(NumpyAverage, self).initialize(func_name=fname)
+        super().initialize(func_name=fname)
 
 
 class MyAverage(PyFuncApp):
+    """
+    Wrapper app turning my_av into a Python function app
+    """
+
     def initialize(self, **kwargs):
         fname = 'dlg.common.reproducibility.apps.my_av'
-        super(MyAverage, self).initialize(func_name=fname)
+        super().initialize(func_name=fname)
