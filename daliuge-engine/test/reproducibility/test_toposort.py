fbad71c5fd2cb22a100a5b029ba7255d5dd7cbe4
pritchardn <21726929@student.uwa.edu.au>
2021-07-21 13:29:01 +0800

    Adds toposorting tests for all graph abstraction levels.

--- daliuge-engine/test/reproducibility/test_toposort.py
+++ daliuge-engine/test/reproducibility/test_toposort.py
@@ -20,7 +20,8 @@
 #    MA 02111-1307  USA
 #
 """
-Tests the correctness of our topological sorting code. This module does not test the final blockDAG generation.
+Tests the correctness of our topological sorting code.
+This module does not test the final blockDAG generation.
 Assumptions:
 - The graphs tests are valid (not malformed)
 """
@@ -28,31 +29,90 @@ Assumptions:
 import json
 import unittest
 
-from dlg.common.reproducibility.reproducibility import init_lgt_repro_data, init_lg_repro_data, lg_build_blockdag
+from dlg.common.reproducibility.reproducibility import \
+    init_lgt_repro_data, init_lg_repro_data, lg_build_blockdag, build_blockdag
+
+_dummydrop = {'oid': 1,
+              'reprodata': {
+                  'rmode': "1",
+                  'lg_blockhash': "123",
+                  'pgt_data': {
+                      'merkleroot': "456"
+                  },
+                  'pgt_parenthashes': {},
+                  'pgt_blockhash': "135",
+                  'pg_data': {
+                      'merkleroot': 'bogus'
+                  },
+                  'pg_parenthashes': {},
+                  'pg_blockhash': "246",
+                  'rg_data': {
+                      'merkleroot': 'bogus2'
+                  },
+                  'rg_parenthashes': {},
+              }
+              }
+
+
+def _init_graph(filename):
+    file = open(filename)
+    lgt = json.load(file)
+    file.close()
+    for drop in lgt['nodeDataArray']:
+        drop['reprodata'] = {}
+        drop['reprodata']['lg_parenthashes'] = []
+        drop['reprodata']['lgt_data'] = {'merkleroot': "1"}
+        drop['reprodata']['lg_data'] = {}
+    return lgt
+
+
+def _init_pgraph_single():
+    return [_dummydrop.copy()]
+
+
+def _init_pgraph_twostart():
+    pgt = [_dummydrop.copy(), _dummydrop.copy(), _dummydrop.copy()]
+    pgt[1]['oid'] = 2
+    pgt[2]['oid'] = 3
+    pgt[0]['outputs'] = [2]
+    pgt[2]['outputs'] = [2]
+    return pgt
+
+
+def _init_pgraph_twoend():
+    pgt = [_dummydrop.copy(), _dummydrop.copy(), _dummydrop.copy()]
+    pgt[1]['oid'] = 2
+    pgt[2]['oid'] = 3
+    pgt[0]['outputs'] = [2, 3]
+    return pgt
+
+
+def _init_pgraph_twolines():
+    pgt = [_dummydrop.copy(), _dummydrop.copy(), _dummydrop.copy(), _dummydrop.copy()]
+    pgt[1]['oid'] = 2
+    pgt[2]['oid'] = 3
+    pgt[3]['oid'] = 4
+    pgt[0]['outputs'] = [2]
+    pgt[2]['outputs'] = [4]
+    return pgt
 
 
 class ToposortTests(unittest.TestCase):
-
-    def init_graph(self, filename):
-        fp = open(filename)
-        lgt = json.load(fp)
-        fp.close()
-        for drop in lgt['nodeDataArray']:
-            drop['reprodata'] = {}
-            drop['reprodata']['lg_parenthashes'] = []
-            drop['reprodata']['lgt_data'] = {'merkleroot': "1"}
-            drop['reprodata']['lg_data'] = {}
-        return lgt
+    """
+    Reads test graphs from /topoGraphs, appends some dummy reprodata then runs through the
+    traversal code.
+    The goal is to make sure these routines execute correct topological sorts.
+    """
 
     def test_lg_blockdag_single(self):
         """
         Tests a single drop
         A
         """
-        lgt = self.init_graph("topoGraphs/testSingle.graph")
+        lgt = _init_graph("topoGraphs/testSingle.graph")
         init_lgt_repro_data(lgt, "1")
         init_lg_repro_data(lgt)
-        leaves, visited = lg_build_blockdag(lgt)
+        visited = lg_build_blockdag(lgt)[1]
         self.assertTrue(visited == [-1])
 
     def test_lg_blockdag_twostart(self):
@@ -62,10 +122,10 @@ class ToposortTests(unittest.TestCase):
              C
         B -->
         """
-        lgt = self.init_graph("topoGraphs/testTwoStart.graph")
+        lgt = _init_graph("topoGraphs/testTwoStart.graph")
         init_lgt_repro_data(lgt, "1")
         init_lg_repro_data(lgt)
-        leaves, visited = lg_build_blockdag(lgt)
+        visited = lg_build_blockdag(lgt)[1]
         self.assertTrue(visited == [-3, -1, -2])
 
     def test_lg_blockdag_twoend(self):
@@ -75,10 +135,10 @@ class ToposortTests(unittest.TestCase):
         A
           --> C
         """
-        lgt = self.init_graph("topoGraphs/testTwoEnd.graph")
+        lgt = _init_graph("topoGraphs/testTwoEnd.graph")
         init_lgt_repro_data(lgt, "1")
         init_lg_repro_data(lgt)
-        leaves, visited = lg_build_blockdag(lgt)
+        visited = lg_build_blockdag(lgt)[1]
         self.assertTrue(visited == [-1, -3, -2])
 
     def test_lg_blockdag_twolines(self):
@@ -87,18 +147,163 @@ class ToposortTests(unittest.TestCase):
         A --> B
         C --> D
         """
-        lgt = self.init_graph("topoGraphs/testTwoLines.graph")
+        lgt = _init_graph("topoGraphs/testTwoLines.graph")
         init_lgt_repro_data(lgt, "1")
         init_lg_repro_data(lgt)
-        leaves, visited = lg_build_blockdag(lgt)
+        visited = lg_build_blockdag(lgt)[1]
         self.assertTrue(visited == [-2, -3, -1, -4])
 
     def test_lg_blockdag_empty(self):
         """
         Tests an empty graph. Should fail gracefully.
         """
-        lgt = self.init_graph("topoGraphs/testEmpty.graph")
+        lgt = _init_graph("topoGraphs/testEmpty.graph")
         init_lgt_repro_data(lgt, "1")
         init_lg_repro_data(lgt)
-        leaves, visited = lg_build_blockdag(lgt)
+        visited = lg_build_blockdag(lgt)[1]
+        self.assertTrue(visited == [])
+
+    def test_pgt_blockdag_single(self):
+        """
+        Tests a single drop
+        1
+        """
+        pgt = _init_pgraph_single()
+        visited = build_blockdag(pgt, 'pgt')[1]
+        self.assertTrue(visited == [1])
+
+    def test_pgt_blockdag_twostart(self):
+        """
+        A graph with two starts
+        1 -->
+             3
+        2 -->
+        """
+        pgt = _init_pgraph_twostart()
+        visited = build_blockdag(pgt, 'pgt')[1]
+        self.assertTrue(visited == [3, 1, 2])
+
+    def test_pgt_blockdag_twoend(self):
+        """
+        A graph with two ends
+          --> 2
+        1
+          --> 3
+        """
+        pgt = _init_pgraph_twoend()
+        visited = build_blockdag(pgt, 'pgt')[1]
+        self.assertTrue(visited == [1, 3, 2])
+
+    def test_pgt_blockdag_twolines(self):
+        """
+        A graph with two starts and two ends
+        1 --> 2
+        3 --> 4
+        """
+        pgt = _init_pgraph_twolines()
+        visited = build_blockdag(pgt, 'pgt')[1]
+        self.assertTrue(visited == [3, 4, 1, 2])
+
+    def test_pgt_blockdag_empty(self):
+        """
+        Tests an empty graph. Should fail gracefully.
+        """
+        pgt = []
+        visited = build_blockdag(pgt, 'pgt')[1]
+        self.assertTrue(visited == [])
+
+    def test_pg_blockdag_single(self):
+        """
+        Tests a single drop
+        """
+        pg = _init_pgraph_single()
+        visited = build_blockdag(pg, 'pg')[1]
+        self.assertTrue(visited == [1])
+
+    def test_pg_blockdag_twostart(self):
+        """
+        A graph with two starts
+        1 -->
+             3
+        2 -->
+        """
+        pg = _init_pgraph_twostart()
+        visited = build_blockdag(pg, 'pg')[1]
+        self.assertTrue(visited == [3, 1, 2])
+
+    def test_pg_blockdag_twoend(self):
+        """
+        A graph with two ends
+          --> 2
+        1
+          --> 3
+        """
+        pg = _init_pgraph_twoend()
+        visited = build_blockdag(pg, 'pg')[1]
+        self.assertTrue(visited == [1, 3, 2])
+
+    def test_pg_blockdag_twolines(self):
+        """
+        A graph with two starts and two ends
+        1 --> 2
+        3 --> 4
+        """
+        pg = _init_pgraph_twolines()
+        visited = build_blockdag(pg, 'pgt')[1]
+        self.assertTrue(visited == [3, 4, 1, 2])
+
+    def test_pg_blockdag_empty(self):
+        """
+        Tests an empty graph. Should fail gracefully.
+        """
+        pg = []
+        visited = build_blockdag(pg, 'pg')[1]
+        self.assertTrue(visited == [])
+
+    def test_rg_blockdag_single(self):
+        """
+        Tests a single drop
+        """
+        rg = _init_pgraph_single()
+        visited = build_blockdag(rg, 'rg')[1]
+        self.assertTrue(visited == [1])
+
+    def test_rg_blockdag_twostart(self):
+        """
+        A graph with two starts
+        1 -->
+             3
+        2 -->
+        """
+        rg = _init_pgraph_twostart()
+        visited = build_blockdag(rg, 'rg')[1]
+        self.assertTrue(visited == [3, 1, 2])
+
+    def test_rg_blockdag_twoend(self):
+        """
+        A graph with two ends
+          --> 2
+        1
+          --> 3
+        """
+        rg = _init_pgraph_twoend()
+        visited = build_blockdag(rg, 'rg')[1]
+        self.assertTrue(visited == [1, 3, 2])
+
+    def test_rg_blockdag_twolines(self):
+        """
+        A graph with two starts and two ends
+        1 --> 2
+        3 --> 4
+        """
+        rg = _init_pgraph_twolines()
+        visited = build_blockdag(rg, 'rg')[1]
+        self.assertTrue(visited == [3, 4, 1, 2])
+
+    def test_rg_blockdag_empty(self):
+        """
+        Tests an empty graph. Should fail gracefully.
+        """
+        rg = []
+        visited = build_blockdag(rg, 'rg')[1]
         self.assertTrue(visited == [])
