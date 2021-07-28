1baf2457028a6f72cdfd5a63cc43da2f222773fd
pritchardn <21726929@student.uwa.edu.au>
2021-07-28 10:43:15 +0800

    Style changes to reproducibility tests for pylint score.

--- daliuge-engine/test/reproducibility/test_drophash.py
+++ daliuge-engine/test/reproducibility/test_drophash.py
@@ -32,57 +32,60 @@ from dlg.drop import AbstractDROP
 from merklelib import MerkleTree
 
 
-class RerunHashTests(unittest.TestCase):
+class AbstractDROPHashTests(unittest.TestCase):
+    """
+    Tests the reprodata generation methods of the AbstractDROP class
+    """
     def test_null_merkleroot(self):
         """
         Sanity check that the default MerkleRoot of an abstract drop is Null
         Consider it a cardinal sin to change this.
         """
-        a = AbstractDROP('a', 'a')
-        self.assertIsNone(a.merkleroot)
+        drop_a = AbstractDROP('a', 'a')
+        self.assertIsNone(drop_a.merkleroot)
 
     def test_generate_rerun_data(self):
         """
         Tests that completed Rerun data contains the completed flag.
         """
-        a = AbstractDROP('a', 'a')
-        a.reproducibility_level = ReproducibilityFlags.RERUN
-        a.setCompleted()
-        self.assertEqual(a.generate_rerun_data(), {'status': DROPStates.COMPLETED})
+        drop_a = AbstractDROP('a', 'a')
+        drop_a.reproducibility_level = ReproducibilityFlags.RERUN
+        drop_a.setCompleted()
+        self.assertEqual(drop_a.generate_rerun_data(), {'status': DROPStates.COMPLETED})
 
     def test_commit_on_complete(self):
         """
         Tests that merkle_data is generated upon set_complete status and is correct (NOTHING, RERUN)
         """
 
-        a = AbstractDROP('a', 'a')
-        b = AbstractDROP('b', 'b')
-        a.reproducibility_level = ReproducibilityFlags.RERUN
-        b.reproducibility_level = ReproducibilityFlags.NOTHING
-        self.assertIsNone(a.merkleroot)
-        self.assertIsNone(b.merkleroot)
+        drop_a = AbstractDROP('a', 'a')
+        drop_b = AbstractDROP('b', 'b')
+        drop_a.reproducibility_level = ReproducibilityFlags.RERUN
+        drop_b.reproducibility_level = ReproducibilityFlags.NOTHING
+        self.assertIsNone(drop_a.merkleroot)
+        self.assertIsNone(drop_b.merkleroot)
 
         # Test RERUN
-        a.setCompleted()
+        drop_a.setCompleted()
         test = MerkleTree({'status': DROPStates.COMPLETED}.items(), common_hash)
         # 689fcf0d74c42200bef177db545adc43c135dfb0d7dc85b166db3af1dcded235
-        self.assertTrue(test.merkle_root == a.merkleroot)
+        self.assertTrue(test.merkle_root == drop_a.merkleroot)
 
         # Test NOTHING
-        b.setCompleted()
+        drop_b.setCompleted()
         # None
-        self.assertIsNone(b.merkleroot)
-        self.assertTrue(b._committed)
+        self.assertIsNone(drop_b.merkleroot)
+        self.assertTrue(drop_b._committed)
 
     def test_recommit(self):
         """
         Should raise an exception preventing a straight-recommit.
         """
-        a = AbstractDROP('a', 'a')
-        a.reproducibility_level = ReproducibilityFlags.RERUN
-        a.setCompleted()
+        drop_a = AbstractDROP('a', 'a')
+        drop_a.reproducibility_level = ReproducibilityFlags.RERUN
+        drop_a.setCompleted()
         with self.assertRaises(Exception):
-            a.commit()
+            drop_a.commit()
 
     def test_set_reproducibility_level(self):
         """
@@ -90,20 +93,20 @@ class RerunHashTests(unittest.TestCase):
         If already committed. The drop should reset and re-commit all reproducibility data
         If not committed, the change can proceed simply.
         """
-        a = AbstractDROP('a', 'a')
-        b = AbstractDROP('b', 'b')
-        a.reproducibility_level = ReproducibilityFlags.NOTHING
-        b.reproducibility_level = ReproducibilityFlags.NOTHING
+        drop_a = AbstractDROP('a', 'a')
+        drop_b = AbstractDROP('b', 'b')
+        drop_a.reproducibility_level = ReproducibilityFlags.NOTHING
+        drop_b.reproducibility_level = ReproducibilityFlags.NOTHING
 
-        a.setCompleted()
-        self.assertIsNone(a.merkleroot)
-        a.reproducibility_level = ReproducibilityFlags.RERUN
-        self.assertIsNotNone(a.merkleroot)
+        drop_a.setCompleted()
+        self.assertIsNone(drop_a.merkleroot)
+        drop_a.reproducibility_level = ReproducibilityFlags.RERUN
+        self.assertIsNotNone(drop_a.merkleroot)
 
-        self.assertIsNone(b.merkleroot)
-        b.reproducibility_level = ReproducibilityFlags.RERUN
-        b.setCompleted()
-        self.assertIsNotNone(b.merkleroot)
+        self.assertIsNone(drop_b.merkleroot)
+        drop_b.reproducibility_level = ReproducibilityFlags.RERUN
+        drop_b.setCompleted()
+        self.assertIsNotNone(drop_b.merkleroot)
 
         with self.assertRaises(TypeError):
-            a.reproducibility_level = 'REPEAT'
+            drop_a.reproducibility_level = 'REPEAT'
