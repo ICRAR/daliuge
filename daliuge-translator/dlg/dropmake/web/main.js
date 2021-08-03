ccf6e094f0d827e437b934e9ed3d12ba65f69e1f
M-Wicenec <wicenecmw@gmail.com>
2021-08-03 12:09:34 +0800

    skipped colour in graphadded again

--- daliuge-translator/dlg/dropmake/web/main.js
+++ daliuge-translator/dlg/dropmake/web/main.js
@@ -90,7 +90,7 @@ function fillOutSettings(){
 
       var blob = new Blob( [ asArray.buffer ], {type: "image/png"} );
       saveAs(blob, pgtName+"_Template.png");
-  });
+    });
   }
 
   function createZipFilename(graph_name){
@@ -129,11 +129,4 @@ function fillOutSettings(){
 
   function zoomToFit() {
     myDiagram.zoomToFit()
-    // console.log(myDiagram.viewportBounds.width.toString());
-    // console.log('\n');
-    // console.log(myDiagram.viewportBounds.height.toString());
-    // console.log('\n -----');
-    // console.log(myDiagram.documentBounds.width.toString());
-    // console.log('\n');
-    // console.log(myDiagram.documentBounds.height.toString());
   }
