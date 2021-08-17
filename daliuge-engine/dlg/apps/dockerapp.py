9538d3cc209bfc0a4e1c6174eac43fc98cc60706
cgray <cal.j.gray@gmail.com>
2021-08-17 12:44:26 +0800

    conditionally remove containers
    Former-commit-id: 4d2a04613a81388da5043cba5bb8fa8b93bdb82a
    Former-commit-id: 6ef728ce69b3e82c8681e900577eea69b948bf88

--- daliuge-engine/dlg/apps/dockerapp.py
+++ daliuge-engine/dlg/apps/dockerapp.py
@@ -364,7 +364,7 @@ class DockerApp(BarrierAppDROP):
                 user=user,
                 environment=env,
                 working_dir=self.workdir,
-                auto_remove=True
+                auto_remove=self._removeContainer
         )
         self._containerId = cId = container.id
         logger.info("Created container %s for %r", cId, self)
