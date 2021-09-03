edda88f6c743a7d621ae6f78cf938d0d32cdffbb
awicenec <awicenec@gmail.com>
2021-09-03 16:34:27 +0800

    Introduced a small typo

--- daliuge-engine/dlg/deploy/pawsey/scale_test.py
+++ daliuge-engine/dlg/deploy/pawsey/scale_test.py
@@ -80,7 +80,7 @@ class DefaultConfig(object):
 
 class GalaxyMWAConfig(DefaultConfig):
     def __init__(self):
-        super(GalaxyMWAConfig, self).__init__()P
+        super(GalaxyMWAConfig, self).__init__()
 
     def init_list(self):
         return ['mwaops', '/group/mwaops/cwu/dfms/logs']
