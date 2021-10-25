a063c12ea2bbbd099bf53cfce300bf75cc7fc9b3
Andreas Wicenec <awicenec@gmail.com>
2021-10-25 17:06:16 +0800

    added ipython to requirements.txt
    Former-commit-id: 4920b441cf7bbe590b566ac64e47a829dfc316a3
    Former-commit-id: ce919e6e83b8e15174e34cb19b93b676f142fb95

--- docs/conf.py
+++ docs/conf.py
@@ -79,6 +79,8 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.imgmath',
     'sphinx_rtd_theme',
+    'IPython.sphinxext.ipython_console_highlighting',
+    'IPython.sphinxext.ipython_directive',
 ]
 templates_path = ['_templates']
 source_suffix = ['.rst', '.md']
