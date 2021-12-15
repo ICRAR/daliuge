fc4374ff47a6bc0905a510498e27b5ded44f611c
Andreas Wicenec <awicenec@gmail.com>
2021-12-15 09:40:08 +0800

    fixed directory and file names
    Former-commit-id: 33f9b58dcb4981b5d881ad8f63f17225546cd82a
    Former-commit-id: 21c53dd41e56af10c1d78a1a1cbed84189b563dc

--- daliuge-engine/dlg/deploy/start_dlg_cluster.py
+++ daliuge-engine/dlg/deploy/start_dlg_cluster.py
@@ -315,7 +315,8 @@ def get_pg(opts, nms, dims):
     )
     pg = pg_generator.resource_map(pgt, dims + nms, num_islands=num_dims,
         co_host_dim=opts.co_host_dim)
-    with open(os.path.join(opts.log_dir, "pg.json"), "wt") as f:
+    graph_name = f"{opts.log_dir.split('_')[0]}.json"\  # get just the graph name
+    with open(os.path.join(opts.log_dir, graph_name), "wt") as f:
         json.dump(pg, f)
     return pg
 
