2a64c3186c7609f2d89b75f2c160fa44b570ba1e
Nicholas Pritchard <21726929@student.uwa.edu.au>
2022-01-13 09:39:49 +0800

    refactoring general deployment functions out. Renames slurm_utils to deployment_utils Adds runway for a helm_client
    Former-commit-id: ad2845c3928f9a852eba2292edd36a43d0f07d3b
    Former-commit-id: b545b6bbe01a6e0a3c4633d23fb113e5301b504b

--- daliuge-engine/dlg/deploy/helm_client.py
+++ daliuge-engine/dlg/deploy/helm_client.py
@@ -1,6 +1,6 @@
 #
 #    ICRAR - International Centre for Radio Astronomy Research
-#    (c) UWA - The University of Western Australia, 2019
+#    (c) UWA - The University of Western Australia, 2016
 #    Copyright by UWA (in the framework of the ICRAR)
 #    All rights reserved
 #
@@ -19,21 +19,18 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 #    MA 02111-1307  USA
 #
+"""
+Contains a module translating physical graphs to kubernetes helm charts.
+"""
 
-import unittest
 
-from dlg.deploy import deployment_utils
+class HelmClient:
+    """
+    TODO: Understanding basic helm charts
+    """
 
+    def create_helm_chart(self, physical_graph_file):
+        pass
 
-class TestSlurmUtils(unittest.TestCase):
-    def assert_list_as_string(self, s, expected_list):
-        slurm_list = deployment_utils.list_as_string(s)
-        self.assertEqual(expected_list, slurm_list)
-
-    def test_list_as_string(self):
-        self.assert_list_as_string(
-            "a008,b[072-073,076]", ["a008", "b072", "b073", "b076"]
-        )
-        self.assert_list_as_string(
-            "pleiades[03-05]", ["pleiades03", "pleiades04", "pleiades05"]
-        )
+    def submit_job(self):
+        pass
