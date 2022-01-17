8b0d46c3ab416550237da9f419c947307dcf90e9
Nicholas Pritchard <21726929@student.uwa.edu.au>
2022-01-17 11:47:13 +0800

    Adds test for default helm chart creation.

--- daliuge-engine/test/deploy/test_helm_client.py
+++ daliuge-engine/test/deploy/test_helm_client.py
@@ -23,11 +23,32 @@
 Module tests the helm chart translation and deployment functionality.
 """
 import unittest
+import tempfile
+import os
+import yaml
+
+from dlg.common.version import version as dlg_version
+from dlg.deploy.helm_client import HelmClient
 
 
 class TestHelmClient(unittest.TestCase):
 
-    def test_create_helm_chart(self):
+    def test_create_default_helm_chart(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            helm_client = HelmClient(deploy_dir=tmp_dir)
+            helm_client.create_helm_chart([])
+            print(helm_client._chart_name)
+            chart_file_name = os.path.join(helm_client._chart_dir, "Chart.yaml")
+            print(chart_file_name)
+            with open(chart_file_name, 'r', encoding='utf-8') as chart_file:
+                chart_data = yaml.safe_load(chart_file)
+                self.assertEqual(helm_client._chart_name, chart_data['name'])
+                self.assertEqual(dlg_version, chart_data['appVersion'])
+
+    def test_create_single_node_helm_chart(self):
+        self.fail("Test not yet implemented")
+
+    def test_create_multi_node_helm_chart(self):
         self.fail("Test not yet implemented")
 
     def test_submit_job(self):
