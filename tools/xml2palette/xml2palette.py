fe75425de942697ae193cedf726ed55162da645c
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-02-02 16:03:53 +0800

    Fixed capitalisation of Mpi

--- tools/xml2palette/xml2palette.py
+++ tools/xml2palette/xml2palette.py
@@ -148,7 +148,7 @@ def find_field_by_name(fields, name):
 
 
 def add_required_fields_for_category(fields, category):
-    if category in ["DynlibApp", "PythonApp", "Branch", "BashShellApp", "MPI"]:
+    if category in ["DynlibApp", "PythonApp", "Branch", "BashShellApp", "Mpi"]:
         if find_field_by_name(fields, "execution_time") is None:
             fields.append(
                 create_field(
@@ -220,7 +220,7 @@ def add_required_fields_for_category(fields, category):
                     False,
                 )
             )
-    if category in ["File", "Memory", "NGAS", "ParameterSet", "Plasma", "PlasmaFlight", "S3", "MPI"]:
+    if category in ["File", "Memory", "NGAS", "ParameterSet", "Plasma", "PlasmaFlight", "S3", "Mpi"]:
         if find_field_by_name(fields, "group_end") is None:
             fields.append(
                 create_field(
