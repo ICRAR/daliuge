4b2c72f0eecde2d3e1182854c2f1bfeaf3dfbda9
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-02-08 11:24:09 +0800

    Remove debug comments and unnecessary secondary location for schema
    Former-commit-id: 1e95479121fdbeb0a58b711bab8d1fb85b6c1bdb
    Former-commit-id: 2d0306f3a3ee8d9608498f10ed0ba9cce0091759

--- daliuge-translator/dlg/dropmake/web/lg_web.py
+++ daliuge-translator/dlg/dropmake/web/lg_web.py
@@ -87,7 +87,6 @@ ALGO_PARAMS = [
 ]  # max_mem is only relevant for the old editor, not used in EAGLE
 
 LG_SCHEMA_PATH = "/daliuge/dlg-lg.graph.schema"
-LG_SCHEMA_PATH_LOCAL = "daliuge-translator/dlg-lg.graph.schema"
 
 def lg_path(lg_name):
     return "{0}/{1}".format(lg_dir, lg_name)
@@ -509,22 +508,13 @@ def gen_pgt_post():
     try:
         logical_graph = json.loads(json_string)
 
-        # debug
-        print("os.getcwd()" + str(os.getcwd()))
-        print("os.listdir()" + str(os.listdir()))
-        print("os.path.exists()" + LG_SCHEMA_PATH + " " + str(os.path.exists(LG_SCHEMA_PATH)))
-        print("os.path.exists()" + LG_SCHEMA_PATH_LOCAL + " " + str(os.path.exists(LG_SCHEMA_PATH_LOCAL)))
-
         # load LG schema
         lg_schema = None
         if os.path.exists(LG_SCHEMA_PATH):
             with open(LG_SCHEMA_PATH, "r") as schema_file:
                 lg_schema = json.load(schema_file)
-        elif os.path.exists(LG_SCHEMA_PATH_LOCAL):
-            with open(LG_SCHEMA_PATH_LOCAL, "r") as schema_file:
-                lg_schema = json.load(schema_file)
 
-        # validate JSON
+        # validate JSON (if schema was found)
         if lg_schema is not None:
             validate(logical_graph, lg_schema)
 
