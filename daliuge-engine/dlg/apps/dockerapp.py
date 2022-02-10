3b6a4845bb46477e4cb25907394ceadaf9f3d9e4
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-02-10 14:14:10 +0800

    Added input_error_threshold, n_effective_inputs, n_tries component parameters to appropriate components in the DALiuGE template palette

--- daliuge-engine/dlg/apps/dockerapp.py
+++ daliuge-engine/dlg/apps/dockerapp.py
@@ -99,6 +99,12 @@ class ContainerIpWaiter(object):
 #     \~English Number of cores used
 # @param[in] cparam/group_start Group start/False/Boolean/readwrite/False/
 #     \~English Is this node the start of a group?
+# @param[in] cparam/input_error_threshold "Input error threshold (0 and 100)"/0/Integer/readwrite/False/
+#     \~English Indicates the tolerance to erroneous effective inputs, and after which the application will not be run but moved to the ERROR state
+# @param[in] cparam/n_effective_inputs Number of effective inputs/-1/Integer/readwrite/False/
+#     \~English Application will block until this number of inputs have moved to the COMPLETED state. Special value of -1 means that all inputs are considered as effective
+# @param[in] cparam/n_tries Number of tries/1/Integer/readwrite/False/
+#     \~English Specifies the number of times the 'run' method will be executed before finally giving up
 # @param[in] cparam/user User//String/readwrite/False/
 #     \~English Username of the user who will run the application within the docker image
 # @param[in] cparam/ensureUserAndSwitch Ensure User And Switch/False/Boolean/readwrite/False/
@@ -424,7 +430,7 @@ class DockerApp(BarrierAppDROP):
                 for host_path, container_path in self._additionalBindings.items()
             ]
         binds = list(set(binds))  # make this a unique list else docker complains
-        try: 
+        try:
             binds.remove(':')
         except:
             pass
