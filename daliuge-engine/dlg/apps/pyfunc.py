d741087a7df9cd568417c381e23e584d3115db18
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-02-10 13:49:49 +0800

    Added input_error_threshold, n_effective_inputs, n_tries component parameters to the remainder of the components in the DALiuGE palette
    Former-commit-id: d14e2dd5714e651fa71e6b9258ad9ba4a312c344
    Former-commit-id: 74ebf0c9d0378039b188728cf1848f43f57503e5

--- daliuge-engine/dlg/apps/pyfunc.py
+++ daliuge-engine/dlg/apps/pyfunc.py
@@ -116,6 +116,12 @@ def import_using_code(code):
 #     \~English Number of cores used
 # @param[in] cparam/group_start Group start/False/Boolean/readwrite/False/
 #     \~English Is this node the start of a group?
+# @param[in] cparam/input_error_threshold "Input error threshold (0 and 100)"/0/Integer/readwrite/False/
+#     \~English Indicates the tolerance to erroneous effective inputs, and after which the application will not be run but moved to the ERROR state
+# @param[in] cparam/n_effective_inputs Number of effective inputs/-1/Integer/readwrite/False/
+#     \~English Application will block until this number of inputs have moved to the COMPLETED state. Special value of -1 means that all inputs are considered as effective
+# @param[in] cparam/n_tries Number of tries/1/Integer/readwrite/False/
+#     \~English Specifies the number of times the 'run' method will be executed before finally giving up
 # @param[in] aparam/func_name Function Name//String/readwrite/False/
 #     \~English Python fuction name
 # @param[in] aparam/func_code Function Code//String/readwrite/False/
