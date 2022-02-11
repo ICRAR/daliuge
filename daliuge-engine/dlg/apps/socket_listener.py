0c6965c2fb64eebad4593edadd2be3a0ddc5302a
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-02-11 12:16:43 +0800

    Removed n_effective_inputs Doxygen from all components. Added some missing cparams to the Doxygen for the GenericNpyGatherApp

--- daliuge-engine/dlg/apps/socket_listener.py
+++ daliuge-engine/dlg/apps/socket_listener.py
@@ -67,8 +67,6 @@ logger = logging.getLogger(__name__)
 #     \~English Is this node the start of a group?
 # @param[in] cparam/input_error_threshold "Input error threshold (0 and 100)"/0/Integer/readwrite/False/
 #     \~English Indicates the tolerance to erroneous effective inputs, and after which the application will not be run but moved to the ERROR state
-# @param[in] cparam/n_effective_inputs Number of effective inputs/-1/Integer/readwrite/False/
-#     \~English Application will block until this number of inputs have moved to the COMPLETED state. Special value of -1 means that all inputs are considered as effective
 # @param[in] cparam/n_tries Number of tries/1/Integer/readwrite/False/
 #     \~English Specifies the number of times the 'run' method will be executed before finally giving up
 # @param[in] aparam/host Host/127.0.0.1/String/readwrite/False/
