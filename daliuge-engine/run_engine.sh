5868d251aa0f949c27fc9d66fd773ba5fbfa915e
Andreas Wicenec <awicenec@gmail.com>
2022-02-11 16:47:37 +0800

    Use the non-ray base image by default

--- daliuge-engine/run_engine.sh
+++ daliuge-engine/run_engine.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 DOCKER_OPTS="\
 --shm-size=1g --ipc=shareable \
 --rm \
@@ -7,20 +6,7 @@ DOCKER_OPTS="\
 -p 5555:5555 -p 6666:6666 \
 -p 8000:8000 -p 8001:8001 \
 -p 8002:8002 -p 9000:9000 \
---user $(id -u):$(id -g) \
 " 
-common_prep () {
-    mkdir -p ${DLG_ROOT}/workspace
-    mkdir -p ${DLG_ROOT}/testdata
-    mkdir -p ${DLG_ROOT}/code
-    # get current user and group id and prepare passwd and group files
-    DOCKER_GID=`python3 docker/prepare_user.py`
-    DOCKER_OPTS=${DOCKER_OPTS}" --group-add ${DOCKER_GID}"
-    DOCKER_OPTS=${DOCKER_OPTS}" -v ${DLG_ROOT}/workspace/settings/passwd:/etc/passwd"
-    DOCKER_OPTS=${DOCKER_OPTS}" -v ${DLG_ROOT}/workspace/settings/group:/etc/group"
-    DOCKER_OPTS=${DOCKER_OPTS}" -v ${PWD}/dlg/manager:/dlg/lib/python3.8/site-packages/dlg/manager"
-    DOCKER_OPTS=${DOCKER_OPTS}" -v ${DLG_ROOT}:${DLG_ROOT} --env DLG_ROOT=${DLG_ROOT}"
-}
 
 case "$1" in
     "dep")
@@ -31,17 +17,21 @@ case "$1" in
             echo "Please either create and grant access to $USER or build and run the development version."
         else
             VCS_TAG=`git describe --tags --abbrev=0|sed s/v//`
-            common_prep
+            DOCKER_OPTS=${DOCKER_OPTS}"-v ${DLG_ROOT}:${DLG_ROOT} --env DLG_ROOT=${DLG_ROOT} "
             echo "Running Engine deployment version in background..."
             echo "docker run -td "${DOCKER_OPTS}"  icrar/daliuge-engine:${VCS_TAG}"
             docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}
             exit 0
         fi;;
     "dev")
-        export DLG_ROOT="/tmp/dlg"
+        DLG_ROOT="/tmp/dlg"
         export VCS_TAG=`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`
-        common_prep
         echo "Running Engine development version in background..."
+        mkdir -p ${DLG_ROOT}/workspace
+        mkdir -p ${DLG_ROOT}/testdata
+        mkdir -p ${DLG_ROOT}/code
+        DOCKER_OPTS=${DOCKER_OPTS}"-v ${PWD}/dlg/manager:/root/dlg/lib/python3.8/site-packages/dlg/manager"
+        DOCKER_OPTS=${DOCKER_OPTS}" -v ${DLG_ROOT}:${DLG_ROOT} --env DLG_ROOT=${DLG_ROOT}"
         echo "docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}"
         docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine:${VCS_TAG}
         sleep 3
@@ -51,20 +41,14 @@ case "$1" in
         DLG_ROOT="/tmp/dlg"
         export VCS_TAG=`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`
         echo "Running Engine development version in background..."
-        common_prep
+        mkdir -p ${DLG_ROOT}/workspace
+        mkdir -p ${DLG_ROOT}/testdata
+        mkdir -p ${DLG_ROOT}/code
+        DOCKER_OPTS=${DOCKER_OPTS}"-v ${PWD}/dlg/manager:/root/dlg/lib/python3.8/site-packages/dlg/manager"
+        DOCKER_OPTS=${DOCKER_OPTS}" -v ${DLG_ROOT}:${DLG_ROOT} --env DLG_ROOT=${DLG_ROOT}"
         CONTAINER_NM="icrar/daliuge-engine:${VCS_TAG}-casa"
         echo "docker run -td ${DOCKER_OPTS}  ${CONTAINER_NM}"
         docker run -td ${DOCKER_OPTS}  ${CONTAINER_NM}
-        sleep 3
-        ./start_local_managers.sh
-        exit 0;;
-    "slim")
-        export DLG_ROOT="/tmp/dlg"
-        export VCS_TAG=`git rev-parse --abbrev-ref HEAD | tr '[:upper:]' '[:lower:]'`
-        common_prep
-        echo "Running Engine development version in background..."
-        echo "docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine.slim"
-        docker run -td ${DOCKER_OPTS}  icrar/daliuge-engine.slim
         sleep 3
         ./start_local_managers.sh
         exit 0;;
