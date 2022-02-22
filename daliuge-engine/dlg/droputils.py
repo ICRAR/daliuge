ecfccb40f86a2a567ae192411e81a3822ac3e354
Callan Gray <cal.j.gray@gmail.com>
2022-02-22 15:31:59 +0800

    testing
    Former-commit-id: 9b3610c3d71e3ed60f16206fe6115589546adb0a
    Former-commit-id: f9b39cecd0d1d9d57ce6c4a293ab3007f443813e

--- daliuge-engine/dlg/droputils.py
+++ daliuge-engine/dlg/droputils.py
@@ -303,8 +303,11 @@ def save_numpy(drop, ndarray: np.ndarray, allow_pickle=False):
     Saves a numpy ndarray to a drop in npy format
     """
     bio = io.BytesIO()
+    # np.save accepts a "file-like" object which basically just requires
+    # a .write() method. Try np.save(drop, array)
     np.save(bio, ndarray, allow_pickle=allow_pickle)
     drop.write(bio.getbuffer())
+    drop.close()
 
 
 def load_numpy(drop: DataDROP, allow_pickle=False) -> np.ndarray:
@@ -316,7 +319,6 @@ def load_numpy(drop: DataDROP, allow_pickle=False) -> np.ndarray:
     res = np.load(io.BytesIO(dropio.buffer()), allow_pickle=allow_pickle)
     dropio.close()
     return res
-
 
 def save_jsonp(drop: PathBasedDrop, data: Dict[str, object]):
     with open(drop.path, 'r') as f:
