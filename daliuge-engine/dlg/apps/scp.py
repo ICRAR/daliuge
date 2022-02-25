c3a3c4a6d2b4cca46cdd7931b3a034f5c4c41d60
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-02-25 09:37:51 +0800

    Started to update Doxygen with select-type fields, adding options and positional values to commands.

--- daliuge-engine/dlg/apps/scp.py
+++ daliuge-engine/dlg/apps/scp.py
@@ -46,23 +46,23 @@ from dlg.meta import (
 # @par EAGLE_START
 # @param category PythonApp
 # @param tag daliuge
-# @param[in] cparam/appclass Application Class/dlg.apps.scp.ScpApp/String/readonly/False/
+# @param[in] cparam/appclass Application Class/dlg.apps.scp.ScpApp/String/readonly/False//False/
 #     \~English Application class
-# @param[in] cparam/execution_time Execution Time/5/Float/readonly/False/
+# @param[in] cparam/execution_time Execution Time/5/Float/readonly/False//False/
 #     \~English Estimated execution time
-# @param[in] cparam/num_cpus No. of CPUs/1/Integer/readonly/False/
+# @param[in] cparam/num_cpus No. of CPUs/1/Integer/readonly/False//False/
 #     \~English Number of cores used
-# @param[in] cparam/group_start Group start/False/Boolean/readwrite/False/
+# @param[in] cparam/group_start Group start/False/Boolean/readwrite/False//False/
 #     \~English Is this node the start of a group?
-# @param[in] cparam/input_error_threshold "Input error rate (%)"/0/Integer/readwrite/False/
+# @param[in] cparam/input_error_threshold "Input error rate (%)"/0/Integer/readwrite/False//False/
 #     \~English the allowed failure rate of the inputs (in percent), before this component goes to ERROR state and is not executed
-# @param[in] cparam/n_tries Number of tries/1/Integer/readwrite/False/
+# @param[in] cparam/n_tries Number of tries/1/Integer/readwrite/False//False/
 #     \~English Specifies the number of times the 'run' method will be executed before finally giving up
-# @param[in] aparam/remoteUser Remote User//String/readwrite/False/
+# @param[in] aparam/remoteUser Remote User//String/readwrite/False//False/
 #     \~English Remote user address
-# @param[in] aparam/pkeyPath Private Key Path//String/readwrite/False/
+# @param[in] aparam/pkeyPath Private Key Path//String/readwrite/False//False/
 #     \~English Private key path
-# @param[in] aparam/timeout Timeout//Float/readwrite/False/
+# @param[in] aparam/timeout Timeout//Float/readwrite/False//False/
 #     \~English Connection timeout in seconds
 # @param[in] port/file File/PathBasedDrop/
 #     \~English Input file path
