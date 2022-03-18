a98c64d18f73bed42f52cea42f185fcd02b7d6b3
Nicholas Pritchard <21726929@student.uwa.edu.au>
2022-03-18 16:11:00 +0800

    Adds empty graph init_repro_data tests.

---
+++ daliuge-translator/test/reproducibility/test_repro_inits.py
@@ -0,0 +1,58 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia, 2017
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+"""
+This module tests the top-level reproducibility repro methods for missing entries.
+"""
+import unittest
+from dlg.common.reproducibility.reproducibility import init_lgt_repro_data, init_lg_repro_data, init_pgt_unroll_repro_data, init_pgt_partition_repro_data, init_pg_repro_data
+
+
+class InitReproTest(unittest.TestCase):
+    """
+    Tests top-level repro methods for missing reprodata
+    """
+
+    def test_lgt_init(self):
+        _graph = {}
+        sig = init_lgt_repro_data(_graph, '0')
+        self.assertNotEqual({}, sig['reprodata'])
+
+    def test_lg_init(self):
+        _graph = {}
+        sig = init_lg_repro_data(_graph)
+        self.assertNotEqual({}, sig['reprodata'])
+
+    def test_pgt_unroll_init(self):
+        _graph = [{}]
+        sig = init_pgt_unroll_repro_data(_graph)
+        self.assertIsNone(sig[0]['signature'])
+
+    def test_pgt_partition_init(self):
+        _graph = [{}]
+        sig = init_pgt_partition_repro_data(_graph)
+        self.assertIsNone(sig[0]['signature'])
+
+    def test_pg_init(self):
+        _graph = [{}]
+        sig = init_pg_repro_data(_graph)
+        self.assertIsNone(sig[0]['signature'])
+
