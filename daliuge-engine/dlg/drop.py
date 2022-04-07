f2f58d4abd91b630c2d6472a33627e4e718718d1
Callan Gray <cal.j.gray@gmail.com>
2022-04-07 10:35:45 +0800

    gather function bugfix tested nifty graphs

--- daliuge-engine/dlg/drop.py
+++ daliuge-engine/dlg/drop.py
@@ -2327,7 +2327,7 @@ class BarrierAppDROP(InputFiredAppDROP):
     def initialize(self, **kwargs):
         # Blindly override existing value if any
         kwargs["n_effective_inputs"] = -1
-        super(BarrierAppDROP, self).initialize(**kwargs)
+        super().initialize(**kwargs)
 
 ##
 # @brief Branch
