c8a8b61a6c72f073f89284f135ab7034eb3315ad
Callan Gray <cal.j.gray@gmail.com>
2022-05-24 12:27:46 +0800

    refactor to popArg

--- daliuge-engine/dlg/environmentvar_drop.py
+++ daliuge-engine/dlg/environmentvar_drop.py
@@ -23,10 +23,12 @@ import abc
 import io
 import os
 import json
+import logging
 
 from dlg.drop import AbstractDROP, DEFAULT_INTERNAL_PARAMETERS
 from dlg.io import MemoryIO
 
+logger = logging.getLogger(__name__)
 
 class KeyValueDROP:
     @abc.abstractmethod
@@ -77,6 +79,7 @@ class EnvironmentVarDROP(AbstractDROP, KeyValueDROP):
         """
         Runs through all parameters, putting each into this drop's variable dict
         """
+        logger.warning(f"params {self.parameters}")
         super(EnvironmentVarDROP, self).initialize(**kwargs)
         self._variables = dict()
         self._variables.update(_filter_parameters(self.parameters))
