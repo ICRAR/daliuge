6457f4fb45f1462de749a83a0c4c28749fc34a23
Andreas Wicenec <awicenec@gmail.com>
2023-03-01 06:28:10 +0100

    More compact description

--- daliuge-engine/dlg/data/drops/file.py
+++ daliuge-engine/dlg/data/drops/file.py
@@ -40,11 +40,11 @@ from typing import Union
 # @param category File
 # @param tag daliuge
 # @param filepath File Path//String/ComponentParameter/readwrite//False/False/File path for this file. If it has a \/ at the end it will be treated as a directory name and the filename will the generated. If it does not have a \/, the last part will be treated as a filename. If filepath does not start with \/ (relative path) then the session directory will be pre-pended to make the path absolute.
-# @param check_filepath_exists Check file path exists/False/Boolean/ComponentParameter/readwrite//False/False/Perform a check to make sure the file path exists before proceeding with the application
+# @param check_filepath_exists Check existence/False/Boolean/ComponentParameter/readwrite//False/False/Perform a check to make sure the file path exists before proceeding with the application
 # @param streaming Streaming/False/Boolean/ComponentParameter/readwrite//False/False/Specifies whether this data component streams input and output data
 # @param persist Persist/True/Boolean/ComponentParameter/readwrite//False/False/Specifies whether this data component contains data that should not be deleted after execution
+# @param delete_parent_directory Delete parent/False/Boolean/ComponentParameter/readwrite//False/False/Also delete the parent directory of this file when deleting the file itself
 # @param data_volume Data volume/5/Float/ComponentParameter/readwrite//False/False/Estimated size of the data contained in this node
-# @param delete_parent_directory Delete parent directory/False/Boolean/ComponentParameter/readwrite//False/False/Also delete the parent directory of this file when deleting the file itself
 # @param group_end Group end/False/Boolean/ComponentParameter/readwrite//False/False/Is this node the end of a group?
 # @param dummy dummy//Object/InputPort/readwrite//False/False/Dummy input port
 # @param dummy dummy//Object/OutputPort/readwrite//False/False/Dummy output port
