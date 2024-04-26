de0b52f64fb1ec8bd5b0d0da3d39869fa7ccf832
Andreas Wicenec <awicenec@gmail.com>
2024-04-26 15:00:53 +0800

    Updated regexp for CORS

--- daliuge-engine/dlg/manager/rest.py
+++ daliuge-engine/dlg/manager/rest.py
@@ -80,9 +80,10 @@ def daliuge_aware(func):
                 origin = bottle.request.headers.raw("Origin")
                 logger.debug("CORS request comming from: %s", origin)
                 if origin is None or re.match(
-                    r"http://(dlg-trans.local:80[0-9][0-9]|dlg-trans.icrar.org)", origin
+                    r"(http://dlg-trans.local:80[0-9][0-9]|https://dlg-trans.icrar.org)",
+                    origin,
                 ):
-                    origin = "http://dlg-trans.local:8084"
+                    pass
                 elif re.match(r"http://((localhost)|(127.0.0.1)):80[0-9][0-9]", origin):
                     origin = "http://localhost:8084"
                 bottle.response.headers["Access-Control-Allow-Origin"] = origin
