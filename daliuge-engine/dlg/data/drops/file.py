71395a15569ca87c3387b5c4ea0a3de3cde33452
Ryan Bunney <ryanbunney@fastmail.com>
2024-05-24 15:34:39 +0800

    Add preliminary named-ports for FileDrops

--- daliuge-engine/dlg/data/drops/file.py
+++ daliuge-engine/dlg/data/drops/file.py
@@ -20,6 +20,7 @@
 #    MA 02111-1307  USA
 #
 import errno
+import logging
 import os
 import re
 
@@ -78,6 +79,7 @@ class FileDROP(DataDROP, PathBasedDrop):
         if kwargs["persist"] and "lifespan" not in kwargs:
             kwargs["expireAfterUse"] = False
         self.is_dir = False
+        self._updatedPorts = False
         super().__init__(*args, **kwargs)
 
     def sanitize_paths(self, filepath: str) -> Union[None, str]:
@@ -108,6 +110,10 @@ class FileDROP(DataDROP, PathBasedDrop):
         """
         FileDROP-specific initialization.
         """
+
+        self._setupFilePaths()
+
+    def _setupFilePaths(self):
         filepath = self.parameters.get("filepath", None)
         dirname = None
         filename = None
@@ -158,7 +164,77 @@ class FileDROP(DataDROP, PathBasedDrop):
         self._wio = None
 
     def getIO(self):
+
+        # We need to update named_ports now we have runtime information
+        if not self._updatedPorts:
+            self._map_input_ports_to_params()
+            self._setupFilePaths()
+
         return FileIO(self._path)
+
+    def _map_input_ports_to_params(self):
+        """
+        Map the input ports that are on this FileDrop to the Drop parameters
+
+        This method performs the following steps:
+
+            - Iterate through each producer, finding the portname and value of the
+            port associated with that producers
+
+            - Iterate through the input port names of _this_ drop, and match the UID
+            and name to the producer map, and then getting the value.
+
+            - Finally, match the value of the named input drop with a DROP parameter (
+            if it exists).
+        """
+
+        try:
+            dropInputPorts = self.parameters['producers']
+        except KeyError:
+            logging.debug("No producers available for drop: %s", self.uid)
+            return
+
+        producerPortValueMap = {}  # Map Producer UIDs to a portname
+        finalDropPortMap = {}  # Final mapping of named port to value stored in producer
+
+        for p in self.producers:
+            params = p.parameters['outputs']
+            for param in params:
+                try:
+                    key = list(param.keys())[0]
+                except AttributeError:
+                    logging.debug("Producer %s does not have named ports", p.uid)
+                    continue
+                portName = param[key]
+                portValue = ""
+                producerUid = p.uid
+                if portName in p.parameters:
+                    portValue = p.parameters[param[key]]
+                # TODO This currently only allows 1 UID -> Portname/Value
+                # Investigate UID -> [Portname1:Value1, Portnam2:value2,..,]
+                producerPortValueMap[producerUid] = {"portname": portName,
+                                                     "value": portValue}
+
+        for port in dropInputPorts:
+            try:
+                port.items()
+            except AttributeError:
+                logging.debug("Producer %s does not have named ports", p.uid)
+                continue
+            for uid, portname in port.items():
+                try:
+                    print(uid, portname)
+                    tmp = producerPortValueMap[uid]
+                    if tmp['portname'] == portname:
+                        finalDropPortMap[portname] = tmp['value']
+                except KeyError:
+                    print("Not available")
+
+        for portname in finalDropPortMap:
+            if portname in self.parameters:
+                self.parameters[portname] = finalDropPortMap[portname]
+
+        self._updatedPorts = True
 
     def delete(self):
         super().delete()
