8a4688eacc7534c0f5068ec499b01935c7377af2
Andreas Wicenec <awicenec@gmail.com>
2024-08-14 22:31:07 +0800

    More trial

--- daliuge-engine/test/manager/test_daemon.py
+++ daliuge-engine/test/manager/test_daemon.py
@@ -31,7 +31,7 @@ from dlg import constants
 from dlg.manager.client import MasterManagerClient
 from dlg.manager.proc_daemon import DlgDaemon
 
-_TIMEOUT = 10
+_TIMEOUT = 3
 IDENTITY = lambda x: x
 
 
