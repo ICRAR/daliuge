8f046d6a0c07db6a83a873003ab558f66d1c48d8
Callan Gray <cal.j.gray@gmail.com>
2025-02-05 14:03:51 +0800

    LIU-449: mark python3.11 and python3.12 compatibility

--- daliuge-common/setup.py
+++ daliuge-common/setup.py
@@ -102,6 +102,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     license="LGPLv2+",
     packages=find_packages(),
--- daliuge-engine/setup.py
+++ daliuge-engine/setup.py
@@ -200,6 +200,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     license="LGPLv2+",
     packages=find_packages(exclude=("test", "test.*", "fabfile")),
