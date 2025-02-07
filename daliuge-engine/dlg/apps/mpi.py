d46381b518b8201702579b20b0f23bfcb777c7e5
Ryan Bunney <ryanbunney@fastmail.com>
2025-02-07 17:06:57 +0800

    LIU-444: Resolve unittest failures and polish.

--- daliuge-engine/dlg/apps/mpi.py
+++ daliuge-engine/dlg/apps/mpi.py
@@ -27,6 +27,7 @@ import signal
 import subprocess
 import sys
 
+from mpi4py import MPI
 
 from dlg import utils, droputils
 from dlg.apps.app_base import BarrierAppDROP
@@ -92,7 +93,6 @@ class MPIApp(BarrierAppDROP):
         self._recompute_data = {}
 
     def run(self):
-        from mpi4py import MPI
         logger.debug("Parameters found: %s",
                      json.dumps(self.parameters))
         logger.debug("MPI Inputs: %s; MPI Outputs: %s",
@@ -121,7 +121,7 @@ class MPIApp(BarrierAppDROP):
             cmd = cmd.replace(f"%{key}%", str(value))
 
         # Replace inputs/outputs in command line with paths or data URLs
-        cmd = droputils.replace_placeholders(cmd, fsInputs, fsOutputs)
+        # cmd = droputils.replace_placeholders(cmd, fsInputs, fsOutputs)
 
         # Pass down daliuge-specific information to the subprocesses as environment variables
         env = os.environ.copy()
