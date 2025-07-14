c596d2d2175c94faedeeb0ae6035e827f81746a7
Ryan Bunney <ryanbunney@fastmail.com>
2025-07-14 16:24:33 +0800

    LIU-505: Update EAGLE-test-graphs to correct version

--- daliuge-translator/setup.py
+++ daliuge-translator/setup.py
@@ -129,6 +129,7 @@ install_requires = [
 extra_requires = {
     "test": [
         "pytest",
+        # Toggle comments below if adding new test graphs
         "eagle-test-graphs==0.1.9",
         # "eagle_test_graphs @ git+https://github.com/ICRAR/EAGLE_test_repo@branch-name",
         "np-merklelib",
