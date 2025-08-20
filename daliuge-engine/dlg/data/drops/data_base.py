e2719bbb053be89acc3f4edc4c81eb135f86fceb
Ryan Bunney <ryanbunney@fastmail.com>
2025-08-20 17:35:21 +0800

    LIU-516: Add support for log details on the Data DROPs
    Initially this was introduced just for AppDROPs - I think the reasoning I gave at the time was I wanted to roll out the feature iteratively to give us time to test it out. Not sure if that was a valid reason or not.
    
    Signed-off-by: Ryan Bunney <ryanbunney@fastmail.com>

--- daliuge-engine/dlg/data/drops/data_base.py
+++ daliuge-engine/dlg/data/drops/data_base.py
@@ -50,6 +50,48 @@ except ImportError:
 
 logger = logging.getLogger(f"dlg.{__name__}")
 
+class InstanceLogHandler(logging.Handler):
+    """Custom handler to store logs in-memory per object instance."""
+    def __init__(self, log_storage):
+        super().__init__()
+        self.log_storage = log_storage
+
+    def emit(self, record):
+        """Store log messages in the instance's log storage.
+
+         :param record: The log string we want to add to the log storage
+
+         .. note: We are not interested in actually emitting the log;
+             we are just interested in extracting and storing Record metadata
+        """
+
+        exc = f"{str(record.exc_text)}" if record.exc_text else ""
+        # msg = str(record.message).replace("\n", "<br>")
+        msg = (f"<pre>{record.message.encode('utf-8').decode('unicode_escape')}\n"
+               f"{exc}</pre>")
+        try:
+            rec_time = record.asctime
+        except AttributeError:
+            rec_time = ""
+        self.log_storage.append({ "time":rec_time,
+            "Level": record.levelname,
+            "Module": record.name,
+            "Function/Method": record.funcName,
+            "Line #": record.lineno,
+            "Message": msg,
+        })
+
+class DROPLogFilter(logging.Filter):
+    def __init__(self, uid: str, humanKey: str):
+        super().__init__()
+        self.uid = uid
+        self.humanKey = humanKey
+
+    def filter(self, record):
+        uid = getattr(record, "drop_uid", None)
+        return uid == self.uid or uid == self.humanKey
+
+
 
 ##
 # @brief Data
@@ -80,6 +122,30 @@ class DataDROP(AbstractDROP):
     and `dataURL`, a getter for a data URI uncluding protocol and address
     parsed by function `IOForURL`.
     """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialise the DROP logging apparatus
+        """
+
+        super(DataDROP, self).__init__(*args, **kwargs)
+
+        self.log_storage = []
+
+        self.logger = logging.getLogger(f"{__class__}.{self.uid}")
+        instance_handler = InstanceLogHandler(self.log_storage)
+        instance_handler.addFilter(DROPLogFilter(self.uid, self._humanKey))
+        fmt = ("%(asctime)-15s [%(levelname)5.5s] "
+               "%(name)s#%(funcName)s:%(lineno)s %(message)s")
+        fmt = logging.Formatter(fmt)
+        instance_handler.setFormatter(fmt)
+
+        # Attach instance-specific handler
+        logging.root.addHandler(instance_handler)
+
+        # Ensure logs still propagate to the root logger
+        logger.propagate = True
+
 
     def incrRefCount(self):
         """
@@ -397,6 +463,13 @@ class DataDROP(AbstractDROP):
         for portname in finalDropPortMap:
             if portname in self.parameters:
                 self.parameters[portname] = finalDropPortMap[portname]
+
+    def getLogs(self):
+        """
+        :return: Return the logs stored in the logging handler
+        """
+
+        return self.log_storage
 
     @abstractmethod
     def getIO(self) -> DataIO:
